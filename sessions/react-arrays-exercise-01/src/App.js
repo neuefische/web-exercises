51a8fd6091dda1e217afaaaa7f4c0c047e205730
Gregor Adams <greg@pixelass.com>
2022-05-01 20:42:25 +0200

    feat(react-arrays): add exercise 01
    Migrated from https://codesandbox.io/s/react-arrays-exercise-fruits-start-1ht6lf

---
+++ sessions/react-arrays-exercise-01/src/App.js
@@ -0,0 +1,12 @@
+import Card from './components/Card';
+import './styles.css';
+
+export default function App() {
+	const fruits = [];
+
+	return (
+		<div className="App">
+			<Card text="banana" />
+		</div>
+	);
+}
