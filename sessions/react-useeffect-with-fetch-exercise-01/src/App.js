854df0bd5b811f3c9220e83316476957a4299997
Gregor Adams <greg@pixelass.com>
2022-05-01 21:02:54 +0200

    feat(react-useeffect-with-fetch): add exercise 01
    migrated from https://codesandbox.io/s/exercise-1-useeffect-with-fetch-bmrmnd

---
+++ sessions/react-useeffect-with-fetch-exercise-01/src/App.js
@@ -0,0 +1,34 @@
+/* Exercise:
+Change the code below so that the "loadPokemon" function is only executed
+when the app component is rendered.
+
+Hint: Don't forget the dependency array!
+*/
+
+import './styles.css';
+import { useState } from 'react';
+
+export default function App() {
+	const [pokemon, setPokemon] = useState([]);
+
+	async function loadPokemon() {
+		try {
+			const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+			const data = await response.json();
+			setPokemon(data.results);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	return (
+		<main className="App">
+			<button onClick={loadPokemon}>Load Pokémon</button>
+			<ul>
+				{pokemon.map(({ name }) => (
+					<li key={name}>{name}</li>
+				))}
+			</ul>
+		</main>
+	);
+}
