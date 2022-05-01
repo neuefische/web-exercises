9052c4092fa449b51452e5608a14fa88349944a7
Gregor Adams <greg@pixelass.com>
2022-05-01 21:20:01 +0200

    feat(react-usestate-and-events): add exercise 01
    migrated from
    https://codesandbox.io/s/usestate-onclick-hlfm9w

---
+++ sessions/react-usestate-and-events-exercise-01/src/App.js
@@ -0,0 +1,21 @@
+/*
+Given are two buttons, the first is going to add
+and second one to substract 1.
+1. Write two functions, add() and subtract().
+Log something to the console.
+2. Add your add() and subtract() function
+to the relevant button.
+*/
+
+import './styles.css';
+
+export default function App() {
+	return (
+		<div className="App">
+			<h1>useState and Events</h1>
+			<h2>onClick Exercise</h2>
+			<button>+ 1</button>
+			<button>- 1</button>
+		</div>
+	);
+}
