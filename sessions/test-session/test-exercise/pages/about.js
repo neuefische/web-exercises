acefa5e4edf357bf4cdb3f33b1faa79ba77ef96f
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-05-09 12:00:46 +0200

    feat: add test exercise files

---
+++ sessions/test-session/test-exercise/pages/about.js
@@ -0,0 +1,3 @@
+export default function AboutPage() {
+	return <div>just testing workflow.</div>;
+}
---
+++ sessions/test-session/test-exercise/pages/index.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function IndexPage() {
+	return (
+		<div>
+			Hello World.{' '}
+			<Link href="/about">
+				<a>About</a>
+			</Link>
+			<Link href="/day">
+				<a>Day</a>
+			</Link>
+		</div>
+	);
+}
