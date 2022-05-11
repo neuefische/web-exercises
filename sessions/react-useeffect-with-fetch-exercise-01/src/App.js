814b60c45069e3af27c3b261bddd55abd13cc30f
Gregor Adams <pixelass@users.noreply.github.com>
2022-05-11 15:39:22 +0200

    feat(react-useeffect-with-fetch): add exercise 01 (#10)
    * feat(react-useeffect-with-fetch): add exercise 01
    
    migrated from https://codesandbox.io/s/exercise-1-useeffect-with-fetch-bmrmnd
    
    * Update App.js
    
    Add student suggestion to exercise text.
    
    Co-authored-by: Felix Kirchhoff <59687330+F-Kirchhoff@users.noreply.github.com>

---
+++ sessions/react-useeffect-with-fetch-exercise-01/src/App.js
@@ -0,0 +1,36 @@
+/* Exercise:
+For now the pokemon are only fetched when we click the button below. Lets change the code such our app does this automatically.
+	- Remove the 'Load Pokemon' button.
+	- Change the code below so that the "loadPokemon" function is only executed
+	when the app component is rendered.
+
+Hint: Don't forget the dependency array!
+*/
+
+import './styles.css';
+import { useState } from 'react';
+
+export default function App() {
+	const [pokemon, setPokemon] = useState([]);
+
+	async function loadPokemon() {
+		try {
+			const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+			const data = await response.json();
+			setPokemon(data.results);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	return (
+		<main className="App">
+			<button onClick={loadPokemon}>Load Pokémon</button>
+			<ul>
+				{pokemon.map(({ name }) => (
+					<li key={name}>{name}</li>
+				))}
+			</ul>
+		</main>
+	);
+}
