6e5264e2199857a3a51b57917479d6e51e8c03f2
Gregor Adams <pixelass@users.noreply.github.com>
2022-05-11 14:21:11 +0200

    feat(js-array-methods): add map exercise (#1)

---
+++ sessions/js-array-methods/map/index.js
@@ -0,0 +1,34 @@
+console.clear();
+
+const cards = [
+	{
+		id: '1',
+		isBookmarked: false,
+		question: 'How often can I use <header>?',
+		answer: 'As often as you like.',
+		tags: ['html', 'elements', 'basic'],
+	},
+	{
+		id: '2',
+		isBookmarked: false,
+		question: 'How often can I use <aside>?',
+		answer: 'As often as you like.',
+		tags: ['html', 'elements', 'basic'],
+	},
+	{
+		id: '3',
+		isBookmarked: true,
+		question: 'On which types can I use destructuring assignment?',
+		answer: 'On Objects and Arrays',
+		tags: ['js', 'next', 'advanced'],
+	},
+];
+
+// 1: Create an array of lower case answer strings.
+const lowerCaseAnswers = null;
+
+// 2: Create an array of strings that are concatenated questions and answers
+const questionsAndAnswersTogether = null;
+
+// 3: Create an array of objects with the two properties question and answer.
+const questionAndAnswer = null;
