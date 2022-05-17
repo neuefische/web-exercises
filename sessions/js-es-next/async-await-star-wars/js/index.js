e26a42236ef72de483c9fb3869cfab979879193b
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-05-17 15:55:27 +0200

    feat: add async-await-star-wars content

--- sessions/js-es-next/async-await-star-wars/js/index.js
+++ sessions/js-es-next/async-await-star-wars/js/index.js
@@ -1,3 +1,35 @@
 console.clear();
 
-console.log('It works!');
+/*
+Exercise:
+
+Rewrite the code below to change the fetch to an async/await function and apply the destructuring assignment for the createPeopleList function.
+
+Don't forget to include the try...catch statement in your async/await function!
+*/
+
+const apiUrl = 'https://swapi.dev/api/people';
+
+fetch(apiUrl)
+	.then(response => response.json())
+	.then(data => createPeopleList(data.results));
+
+function createPeopleList(people) {
+	const listElement = document.createElement('ul');
+	listElement.className = 'tag-list';
+	document.body.append(listElement);
+
+	people.forEach(person => {
+		const item = document.createElement('li');
+		item.className = 'tag';
+		item.innerHTML = `
+    <h2>${person.name}</h2>
+    <ul>
+      <li>Hair: ${person.hair_color}</li>
+      <li>Skin: ${person.skin_color}</li>
+      <li>Eye: ${person.eye}</li>
+    </ul>
+    `;
+		listElement.append(item);
+	});
+}
