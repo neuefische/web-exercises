26deb1c8259dd5c94941c72814ad10eec60b3503
Anita Schiller <anita.s1@gmx.de>
2022-06-15 15:14:30 +0200

    feat(js-forms): add form submit exercise

---
+++ sessions/js-forms/form-submit/js/index.js
@@ -0,0 +1,13 @@
+console.clear();
+
+/*
+Exercise: Form submit
+
+- Add an event listener for the submit event of the form.
+- Get the data of the different input elements, put it in an object and push it into the data array that's already defined for you in the exercise.
+- After every submit, reset the form so all the inputs are empty again.
+
+Important!: Make sure you cancel the default browser behaviour of the formState.
+ */
+
+const data = [];
