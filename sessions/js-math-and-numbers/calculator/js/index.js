331b545253777eeb50374d56b2c442b92d4eded7
Alexander Claes <hello@aclaes.com>
2022-07-05 16:15:08 +0200

    feat(js-math-and-numbers): add calculator exercise

---
+++ sessions/js-math-and-numbers/calculator/js/index.js
@@ -0,0 +1,10 @@
+/*
+Exercise: Implement the calculator funtionality!
+
+- [ ] The add-button should add the two numbers written in the input fields.
+- [ ] The subtract-button should subtract the two numbers written in the input fields.
+- [ ] The mutiply-button should mutiply the two numbers written in the input fields.
+- [ ] The divide-button should divide the two numbers written in the input fields.
+- [ ] Make sure that dividing by 0 is not possible and the user gets informed (e.g. write warning into the page or console.log).
+- [ ] The random-button should fill both input fields with a random value between 1 and 100
+*/
