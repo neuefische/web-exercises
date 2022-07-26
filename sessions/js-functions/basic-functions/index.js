7423c4ad846e05326ec386873c5fc8edd9649d2d
ThomasF85 <thomas.foeldi@gmail.com>
2022-07-26 15:22:38 +0200

    feat(js-functions): add first four exercises

---
+++ sessions/js-functions/basic-functions/index.js
@@ -0,0 +1,35 @@
+console.clear();
+
+/*
+1: Create the data for a little social media post. Create variables for the following data using the const keyword:
+  - a title
+  - a text
+  - the number of likes
+  - the user who created the post
+  - a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
+
+/*
+2: Log all variables to the console. Then increase the likes by one and log all variables again. Adapt your code from 1.1 if neccesary.
+Hint: to increase a value you can use following syntax: n = n + 1
+*/
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
+
+/*
+3: This code is quite repetitive and hard to read.
+ - Write a function called logData which console logs all
+   variables when it is called.
+ - Then replace the console logs above with this function.
+ - Now increase the number of likes 2 more times and log the data after each increase.
+*/
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
