aeadf97c5b1e01b121b638206159e00a2c37be86
Alexander Claes <hello@aclaes.com>
2022-07-26 14:26:25 +0200

    feat(js-basics): update console-methods exercise

--- sessions/js-basics/console-methods/js/index.js
+++ sessions/js-basics/console-methods/js/index.js
@@ -4,20 +4,16 @@ console.clear();
 
 // In this exercise we want to practice some console methods.
 
-// 1. Store the sentence 'Happy coding!' in a variable with the name 'happy'.
+// 1. Store the sentence 'Happy coding!' in a variable with the name 'coding'.
 
-// 2. Use the right way to output the data of the variable 'happy' as a message in the console.
+// 2. Use the right way to output the data of the variable 'coding' as a message in the console.
 
-// 3. Now store the value 'Happy hacking!' in the variable 'happy' and use the right method to output the variable in the console.
+// 3. Now store the value 'Happy hacking!' in the variable 'hacking' and use the right method to output the variable in the console.
 
 // 4. We want to get rid of all messages in our console. Do you remember the right method to clear your console?
 
-// 5. Next step is to store 'This is an error!' in the variable 'error'.
+// 5. Next step is to store the text 'This is an error!' in the variable 'error'.
 
-// 6. Somehow we want to update the value of the variable 'error' with a better error message. Store the value 'I'm a better error message!' in your variable 'error'.
-
-// 7. How do you know that the new value is "I'm a better error message!"? Choose the right console method.
-
-// 8. Now we want that the value of our error variable is shown as a error message in the console. Choose the right console method.
+// 6. Now we want that the value of our error variable is shown as a error message in the console. Choose the right console method.
 
 // Hooray! You reached the end of your exercise!
