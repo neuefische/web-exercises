01acd5f639c04bfcc8ead4cf90fca013f861c772
Alexander Claes <hello@aclaes.com>
2022-07-26 13:48:44 +0200

    feat(js-variables-and-numbers): add social media post exercise

---
+++ sessions/js-variables-and-numbers/social-media-post/js/index.js
@@ -0,0 +1,22 @@
+console.clear();
+
+/*
+1. Create the data for a little social media post. Create variables for the following data using the const keyword:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Then increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
