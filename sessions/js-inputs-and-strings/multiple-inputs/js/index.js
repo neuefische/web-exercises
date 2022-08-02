41990b689518eefade04eddce91f154736aeadc1
anitaschiller <77627311+anitaschiller@users.noreply.github.com>
2022-08-02 16:16:14 +0200

    Feature/js inputs and strings (#37)
    * feat(js-inputs-and-strings): add exercises
    
    * feat(js-input-and-strings): add exercises 3 and 4
    
    * feat(js-input-and-strings): update exercises
    
    * feat(js-input-and-strings): add advanced exercise
    
    * update js-input-and-strings challenges
    
    * feat(js-inputs-and-strings): implement requested changes from review
    
    * feat(js-inputs-and-strings): update package names
    
    Co-authored-by: Mareike Bosselmann <mareike.bosselmann@gmx.de>
    Co-authored-by: Jessica Lörs <jessica.loers@gmail.com>
    Co-authored-by: Andrea Hohnsen <a.hohnsen@mail.de>

---
+++ sessions/js-inputs-and-strings/multiple-inputs/js/index.js
@@ -0,0 +1,16 @@
+/*
+
+Important: Create a fork for each sub challenge!
+Implement the following functionality:
+
+1. On button click: The value of the first input field is copied into the second input field
+
+2. On button click: The value of the first input field is copied in uppercase into the second input field
+
+3. On button click: The values of the two input fields switch.
+
+*/
+
+const firstInput = document.querySelector('[data-js=first-input]');
+const secondInput = document.querySelector('[data-js=second-input]');
+const button = document.querySelector('[data-js=button]');
---
+++ sessions/js-inputs-and-strings/switch-values/js/index.js
@@ -0,0 +1,26 @@
+/*
+
+Switch the input values of the two input fields when clicking the button
+
+- [ ] Add a second input element to the index.html
+- [ ] Assign the second input element to a variable
+- [ ] Add a third button (a switch button) to the index.html
+- [ ] Assign the third button to a variable
+- [ ] Make sure that when the switch button is clicked, the values of the two input fields are being switched
+- [ ] Make sure that all three buttons work on both input fields
+
+Hint: the HTML entity for the switch button is: &updownarrow
+
+*/
+
+const firstInput = document.querySelector('[data-js="first-input"]');
+const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+
+uppercaseButton.addEventListener('click', () => {
+	firstInput.value = firstInput.value.toUpperCase();
+});
+
+lowercaseButton.addEventListener('click', () => {
+	firstInput.value = firstInput.value.toLowerCase();
+});
