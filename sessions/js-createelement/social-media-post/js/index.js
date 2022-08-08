6c41067041e6df4440f91fa5a59c89fccbd0507c
Alex Claes <hello-github@aclaes.com>
2022-08-08 11:16:36 +0200

    feat(js-createelement): add new exercises (#51)
    * feat(js-createelement): add new exercises
    
    * feat(js-createelement): update exercises after code review
    
    * feat(js-createelement): update exercises after code review, part 2
    
    * feat(js-createelement): update exercises after code review, part 3
    
    Co-authored-by: Alexander Claes <hello@aclaes.com>

---
+++ sessions/js-createelement/social-media-post/js/index.js
@@ -0,0 +1,12 @@
+console.clear();
+
+function handleLikeButtonClick(event) {
+	const buttonElement = event.target;
+	buttonElement.classList.toggle('post__button--liked');
+}
+
+const likeButton = document.querySelector('[data-js="like-button"]');
+likeButton.addEventListener('click', handleLikeButtonClick);
+
+// Exercise:
+// Use document.createElement() and append another social media post to the body.
