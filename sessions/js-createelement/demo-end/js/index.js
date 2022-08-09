a6b2c2d73f7d1cd27f9702e3cffda4021b8afc4c
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-08-09 11:41:31 +0200

    feat: add demo code

--- sessions/js-createelement/demo-end/js/index.js
+++ sessions/js-createelement/demo-end/js/index.js
@@ -3,3 +3,15 @@ console.clear();
 const form = document.querySelector('[data-js="card-form"]');
 const textInput = document.querySelector('[data-js="text-input"]');
 const cardContainer = document.querySelector('[data-js="card-container"]');
+
+form.addEventListener('submit', event => {
+	event.preventDefault();
+	const newCard = document.createElement('li');
+	const cardText = textInput.value;
+	newCard.classList.add('card');
+	newCard.innerHTML = `
+		<h2>Card</h2>
+		<p>${cardText}</p>
+	`;
+	cardContainer.append(newCard);
+});
