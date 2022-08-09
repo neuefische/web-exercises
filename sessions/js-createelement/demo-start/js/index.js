19e4317c6ea5c7af1f22003104cd75db51d6f20d
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-08-09 11:32:28 +0200

    refactor: rename folder

---
+++ sessions/js-createelement/demo-start/js/index.js
@@ -0,0 +1,5 @@
+console.clear();
+
+const form = document.querySelector('[data-js="card-form"]');
+const textInput = document.querySelector('[data-js="text-input"]');
+const cardContainer = document.querySelector('[data-js="card-container"]');
