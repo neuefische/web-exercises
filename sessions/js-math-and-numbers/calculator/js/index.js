adfc15e0e5df1427f31f86d7f1941b5e1292416a
Alex Claes <hello-github@aclaes.com>
2022-08-10 11:10:55 +0200

    feat(js-math-and-numbers): add calculator exercise (#41)
    * feat(js-math-and-numbers): add calculator exercise
    
    * feat(js-math-and-numbers): add counter exercise
    
    * feat(js-math-and-numbers): fix bug in calculator exercise
    
    * Update sessions/js-math-and-numbers/calculator/index.html
    
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>
    
    * Update sessions/js-math-and-numbers/counter/index.html
    
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>
    
    * Update sessions/js-math-and-numbers/calculator/js/index.js
    
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>
    
    * Update sessions/js-math-and-numbers/counter/js/index.js
    
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>
    
    * Update sessions/js-math-and-numbers/calculator/js/index.js
    
    Co-authored-by: Thomas Kirst <thomas.kirst@gmail.com>
    
    * feat(js-math-and-numbers): update exercises after code review
    
    Co-authored-by: Alexander Claes <hello@aclaes.com>
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>
    Co-authored-by: Thomas Kirst <thomas.kirst@gmail.com>

---
+++ sessions/js-math-and-numbers/calculator/js/index.js
@@ -0,0 +1,21 @@
+/*
+Exercise: Implement the calculator functionality!
+
+- [ ] All results should be stored in the element holding the result
+- [ ] The add-button should add the two numbers written in the input fields.
+- [ ] The subtract-button should subtract the two numbers written in the input fields.
+- [ ] The multiply-button should multiply the two numbers written in the input fields.
+- [ ] The divide-button should divide the two numbers written in the input fields.
+- [ ] Make sure that dividing by 0 is not possible and the user gets informed (e.g. write warning into the page or console.log).
+- [ ] The random-button should fill both input fields with a random value between 1 and 100
+*/
+
+const resultOutput = document.querySelector('[data-js="result"]');
+const addButton = document.querySelector('[data-js="add-button"]');
+const subtractButton = document.querySelector('[data-js="subtract-button"]');
+const multiplyButton = document.querySelector('[data-js="multiply-button"]');
+const divideButton = document.querySelector('[data-js="divide-button"]');
+const randomButton = document.querySelector('[data-js="random-button"]');
+const firstInput = document.querySelector('[data-js="input1"]');
+const secondInput = document.querySelector('[data-js="input2"]');
+
