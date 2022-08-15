9f4a4054276fd9b44d5bd5f4694e246e208be303
Alex Claes <hello-github@aclaes.com>
2022-08-15 20:47:08 +0200

    Update sessions/js-forms/form-submit/js/index.js
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>

--- sessions/js-forms/form-submit/js/index.js
+++ sessions/js-forms/form-submit/js/index.js
@@ -7,7 +7,7 @@ Exercise: Form submit
 - Get the data of the different input elements, put it in an object and push it to the 'data' array.
 - After every submit, reset the form so all the inputs are empty again.
 
-Important!: Make sure you cancel the default browser behaviour of the formState.
+Important!: Make sure you cancel the default browser behavior of the submit event
  */
 
 const data = [];
