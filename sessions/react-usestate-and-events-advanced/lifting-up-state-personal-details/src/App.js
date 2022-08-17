002640baeb8b313f1a2986c54ceae1cc8eaeb3e8
Felix Kirchhoff <59687330+F-Kirchhoff@users.noreply.github.com>
2022-08-17 15:35:16 +0200

    Apply suggestions from code review
    Co-authored-by: Alex Claes <hello-github@aclaes.com>

--- sessions/react-usestate-and-events-advanced/lifting-up-state-personal-details/src/App.js
+++ sessions/react-usestate-and-events-advanced/lifting-up-state-personal-details/src/App.js
@@ -1,17 +1,17 @@
 /**
 In your Form component, there are two states, "name" and "email",
-which are set via a form submit.
-But we want to use the states somewhere else in our App, namely we
-want to display them inside our App.js under "Your details." For that we
+which are set on form submit.
+Our goal is to use the states somewhere else in our App.
+We want to display them inside our App.js under "Your details." For that we
 need to lift up both states.
 
 1. Lift up both states from your Form to your App component and replace the
-   variables name and email with them.
-2. In your App.js, write a function createUser(name, email)
+   variables `name` and `email` with them.
+2. In your App.js, write a function `createUser(name, email)`
    where you call the set functions of both states to update them.
-3. Pass createUser() as an `onCreateUser` prop to your Form component.
+3. Pass `createUser()` as an `onCreateUser` prop to your Form component.
    Make sure to accept `onCreateUser` in your Form as well.
-4. Inside the Form's handleSubmit(), replace setName() and setEmail()
+4. Inside the Form's `handleSubmit()`, replace `setName()` and `setEmail()`
    with your newly accepted prop `onCreateUser()`.
    Make sure to pass the correct values to this function.
 
@@ -20,8 +20,9 @@ a second time. Outsource the display of the personal details in a new component:
 
  1. Create a new file "DetailsDisplay.js" and inside it write a component with the same name.
  2. Move the jsx elements regarding the personal details display into that component.
- 3. Import and include the component in your App.js.
- 4. Pass down the states as props called "name" and "email". Don't forget to
+     Don't forget that jsx always requires a single root-element.
+ 3. Import and include the component in your "App.js".
+ 4. Pass down the states as props called `name` and `email`. Don't forget to
     accept the props inside your component.
 
 Now we can use the values we get as inputs also in other components. Amazing!
--- sessions/react-usestate-and-events-advanced/lifting-up-state-personal-details/src/Form.js
+++ sessions/react-usestate-and-events-advanced/lifting-up-state-personal-details/src/Form.js
@@ -1,25 +1,25 @@
 import {useState} from 'react';
 
-export default function Form({onUserCreate}) {
+export default function Form() {
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 
 	function handleSubmit(event) {
 		event.preventDefault();
 		const form = event.target;
-		const {name, email} = form.elements;
-		setName(name.value);
-		setEmail(email.value);
+		const {userName, userEmail} = form.elements;
+		setName(userName.value);
+		setEmail(userEmail.value);
 		form.reset();
 	}
 
 	return (
 		<form aria-labelledby="user" onSubmit={handleSubmit}>
 			<h2 id="user">Form with useState</h2>
-			<label htmlFor="name">Name: </label>
-			<input id="name" type="text" placeholder="John Doe" />
-			<label htmlFor="email">Email: </label>
-			<input id="email" type="email" placeholder="john@doe.com" />
+			<label htmlFor="userName">Name: </label>
+			<input id="userName" name="userName" type="text" placeholder="John Doe" />
+			<label htmlFor="userEmail">Email: </label>
+			<input id="userEmail" name="userEmail" type="email" placeholder="john@doe.com" />
 			<button>Submit</button>
 		</form>
 	);
