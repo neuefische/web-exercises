2eacc9403bc19edc33c46164f7ede3b1b08c7e81
Alex Claes <hello-github@aclaes.com>
2022-08-17 13:39:57 +0200

    Update sessions/js-callback-functions/user-login-callback-4-with-multiple-functions/js/index.js
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>

--- sessions/js-callback-functions/user-login-callback-4-with-multiple-functions/js/index.js
+++ sessions/js-callback-functions/user-login-callback-4-with-multiple-functions/js/index.js
@@ -1,7 +1,7 @@
 console.clear();
 
 function handleUserLogin(onSuccess, onError, userName) {
-	if (userName === 'JaneDoe') {
+	if (userName === 'Jane Doe') {
 		onSuccess(userName, 'admin');
 	} else {
 		onError(`Unknown user "${userName}"`);
