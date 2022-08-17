a9e34a4424f617fce996d0cda5af592875d4ec58
Alex Claes <hello-github@aclaes.com>
2022-08-17 13:42:40 +0200

    feat(js-callback-functions): add exercises (#56)
    * feat(js-callback-functions): add exercises
    
    * Update sessions/js-callback-functions/user-login-callback-2-with-argument/js/index.js
    
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>
    
    * Update sessions/js-callback-functions/user-login-callback-3-with-multiple-arguments/js/index.js
    
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>
    
    * Update sessions/js-callback-functions/user-login-callback-4-with-multiple-functions/js/index.js
    
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>
    
    * Update sessions/js-callback-functions/user-login-callback-4-with-multiple-functions/js/index.js
    
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>
    
    * Update sessions/js-callback-functions/user-login-callback-4-with-multiple-functions/js/index.js
    
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>
    
    Co-authored-by: Alexander Claes <hello@aclaes.com>
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>

---
+++ sessions/js-callback-functions/user-login-callback-2-with-argument/js/index.js
@@ -0,0 +1,22 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+	onSuccess('Jane Doe');
+}
+
+// The exercise starts here!
+
+// You should write a callback function in two different ways.
+// In both cases your callback function should accept one parameter: "userName".
+// The following message should be logged to the console:
+// "Welcome ${userName}! You are logged in now."
+
+// 1.
+// Write a function with the "function" keyword named "showWelcomeMessage"
+// Call "handleUserLogin" and pass your callback function as the first argument.
+// (You should see the log from your callback message)
+
+// 2.
+// Write an anonymous function with the fat arrow syntax.
+// Call "handleUserLogin" and pass your callback function as the first argument.
+// (You should see the log from your callback message)
---
+++ sessions/js-callback-functions/user-login-callback-4-with-multiple-functions/js/index.js
@@ -0,0 +1,37 @@
+console.clear();
+
+function handleUserLogin(onSuccess, onError, userName) {
+	if (userName === 'Jane Doe') {
+		onSuccess(userName, 'admin');
+	} else {
+		onError(`Unknown user "${userName}"`);
+	}
+}
+
+// The exercise starts here!
+
+// 1.
+// Write a function with the "function" keyword named "showWelcomeMessage".
+// The function should accept two parameters: "userName" and "userRole".
+// The function should log the following message to the console:
+// "Welcome ${userName}! You are logged in now as ${userRole}."
+
+// 2.
+// Write a function with the "function" keyword named "showErrorMessage".
+// The function should accept one parameter "errorMessage"
+// The function should log the following message to the console:
+// "Login error: ${errorMessage}"
+
+// 3.
+// Call the function "handleUserLogin" and pass three arguments in this order:
+// - callback function: "showWelcomeMessage"
+// - callback function: "showErrorMessage"
+// - user name string: "Jane Doe"
+// (You should see the log from "showWelcomeMessage" in the console)
+
+// 4.
+// Call the function "handleUserLogin" and pass three arguments in this order:
+// - callback function: "showWelcomeMessage"
+// - callback function: "showErrorMessage"
+// - user name string: "John Doe"
+// (You should see the log from "showErrorMessage" in the console)
