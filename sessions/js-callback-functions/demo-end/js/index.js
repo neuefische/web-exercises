52889139f3dc3e9c69c9a28d782b0a8ed4caa5a6
Alex Claes <hello-github@aclaes.com>
2022-08-18 17:56:45 +0200

    js callback functions: add demo (#59)
    * feat(js-callback-functions): add exercises
    
    * feat(js-callback-function): add demo
    
    Co-authored-by: Alexander Claes <hello@aclaes.com>

---
+++ sessions/js-callback-functions/demo-end/js/index.js
@@ -0,0 +1,58 @@
+console.clear();
+
+const saveButton = document.querySelector('[data-js="save-button"]');
+const cancelButton = document.querySelector('[data-js="cancel-button"]');
+const restoreButton = document.querySelector('[data-js="restore-button"]');
+const skipButton = document.querySelector('[data-js="skip-button"]');
+
+// -- 1 -- Callbacks with buttons
+
+// Anonymous callback function
+saveButton.addEventListener('click', () => {
+	console.log('handle save');
+});
+
+// Named callback function
+function handleCancel() {
+	console.log('handle cancel');
+}
+
+cancelButton.addEventListener('click', handleCancel);
+
+// -- 2 -- Callbacks with buttons and event parameter
+
+// Anonymous callback function
+restoreButton.addEventListener('click', event => {
+	console.log('handle restore');
+	console.log(event);
+});
+
+// Named callback function
+function handleSkip(event) {
+	console.log('handle skip');
+	console.log(event);
+}
+
+skipButton.addEventListener('click', handleSkip);
+
+// -- 3 -- Callbacks without buttons
+
+// Higher order function
+function showText(transformation) {
+	transformation('Changed By The Callback Function');
+}
+
+// Two functions used as callbacks
+function lowercase(text) {
+	const result = text.toLowerCase();
+	console.log(result);
+}
+
+function uppercase(text) {
+	const result = text.toUpperCase();
+	console.log(result);
+}
+
+// Call function and pass callback
+showText(lowercase);
+showText(uppercase);
