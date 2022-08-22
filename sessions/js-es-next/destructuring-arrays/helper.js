f9d4678dda62876399a81e15e6b60f66c5247c70
Stefan Korder <stefan.korder@neuefische.de>
2022-08-22 15:25:58 +0200

    changed the error message and console.log to console.error in checkConditions function

--- sessions/js-es-next/destructuring-arrays/helper.js
+++ sessions/js-es-next/destructuring-arrays/helper.js
@@ -1,18 +1,18 @@
 export default function checkConditions(...conditions) {
-    const allConditionsTrue = conditions.every(
-      ([firstValueToCompare, secondValueToCompare, variableName]) => {
-        if (firstValueToCompare === secondValueToCompare) {
-          return true;
-        } else {
-          console.error(
-            `expected ${variableName} to equal ${secondValueToCompare}, instead ${variableName} is ${firstValueToCompare}.`,
-          );
-          return false;
-        }
-      },
-    );
-    if (allConditionsTrue) {
-      console.log('well done');
-    }
+  const allConditionsTrue = conditions.every(
+    ([firstValueToCompare, secondValueToCompare, variableName]) => {
+      if (firstValueToCompare === secondValueToCompare) {
+        return true;
+      } else {
+        console.error(
+          `expected: ${variableName} equals ${secondValueToCompare}, currently: ${variableName} equals ${firstValueToCompare}.`,
+        );
+        return false;
+      }
+    },
+  );
+  if (allConditionsTrue) {
+    console.log('well done');
   }
+}
   
