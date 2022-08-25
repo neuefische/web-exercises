1cb8bc9e303c7713dbb601210fbe2df19d2f5efb
Stefan Korder <stefan.korder@neuefische.de>
2022-08-25 10:43:55 +0200

    updated objects and arrays exercise

--- sessions/js-objects-and-arrays/objects-and-arrays/index.js
+++ sessions/js-objects-and-arrays/objects-and-arrays/index.js
@@ -1,24 +1,20 @@
-import checkConditions from './helper';
-
 console.clear();
 
 const people = [
   {
     name: 'Homer',
     age: 39,
-    parents: [{mothersName: 'Mona', fathersName: 'Abe'}],
-    pet: [{animal: 'dog', name: "Santa's Little Helper"}],
-    friend: [{name: 'Moe Szyslak', work: 'owner of Moes Tavern'}],
+    parents: {mothersName: 'Mona', fathersName: 'Abe'},
+    pet: {animal: 'dog', name: "Santa's Little Helper"},
+    friend: {name: 'Moe Szyslak', work: 'owner of Moes Tavern'},
     'other freinds': ['Barney Gumble', 'Carl Carlson', 'Lenny Leonard'],
     color_cloth: 'white',
   },
   {
     name: 'Marge',
     age: 36,
-    parents: [
-      {mothersName: 'Jacqueline Bouvier', fathersName: 'Clancy Bouvier'},
-    ],
-    pet: [{animal: 'dog', name: "Santa's Little Helper"}],
+    parents: {mothersName: 'Jacqueline Bouvier', fathersName: 'Clancy Bouvier'},
+    pet: {animal: 'dog', name: "Santa's Little Helper"},
     kids: [
       {name: 'Bart', child_number: 'first'},
       {name: 'Lisa', child_number: 'second'},
@@ -29,94 +25,54 @@ const people = [
   {
     name: 'Bart',
     age: 10,
-    parents: [{mothersName: 'Marge', fathersName: 'Homer'}],
-    pet: [{animal: 'dog', name: "Santa's Little Helper"}],
-    quotes: [
-      {first: 'Ay Caramba!', second: 'Eat my shorts!', third: 'I didnt do it!'},
-    ],
+    parents: {mothersName: 'Marge', fathersName: 'Homer'},
+    pet: {animal: 'dog', name: "Santa's Little Helper"},
+    quotes: ['Ay Caramba!', 'Eat my shorts!', 'I didnt do it!'],
     color_cloth: 'red',
   },
   {
     name: 'Lisa',
     age: 8,
-    parents: [{mothersName: 'Marge', fathersName: 'Homer'}],
-    pet: [{animal: 'dog', name: "Santa's Little Helper"}],
+    parents: {mothersName: 'Marge', fathersName: 'Homer'},
+    pet: {animal: 'dog', name: "Santa's Little Helper"},
     quotes: [
-      {
-        first: 'Bart!',
-        second: 'If anyone wants me, Ill be in my room.',
-        third: 'Quit it, Bart!',
-      },
+      'Bart!',
+      'If anyone wants me, Ill be in my room.',
+      'Quit it, Bart!',
     ],
     color_cloth: 'orange',
   },
 ];
 
-{
-  console.log('EXERCISE 1');
-  // Change the value of the variable name to equal the name of the first person.
+// EXERCISE 1
+// Change the value of the variable nameOfFirstPerson to equal the name of the first person.
 
-  // --v-- write code here --v--
+const nameOfFirstPerson = 'change me';
 
-  const name = people;
+// EXERCISE 2
+// Change the value of the variable otherFriends to equal the other friends of Homer.
 
-  // --^-- write code here --^--
+const homersOtherFriends = 'change me';
 
-  checkConditions([name, 'Homer', 'name']);
-}
+// EXERCISE 3
+// Change the value of the variable age to equal the age of the third person.
 
-{
-  console.log('EXERCISE 2');
-  // Change the value of the variable otherFriends to equal the other friends of Homer.
+const ageOfThirdPerson = 'change me';
 
-  // --v-- write code here --v--
+// EXERCISE 4
+// Change the value of the variable mothersName to equal the mothers name of the second person.
 
-  const otherFriends = people;
+const mothersNameOfSecondPerson = 'change me';
 
-  // --^-- write code here --^--
+// EXERCISE 5
+// Change the value of the variable animaltype to equal the type of animal of Lisas pet.
 
-  checkConditions([
-    otherFriends.toString(),
-    ['Barney Gumble', 'Carl Carlson', 'Lenny Leonard'].toString(),
-    'otherFriends',
-  ]);
-}
+const animaltypeOfLisasPet = 'change me';
 
-{
-  console.log('EXERCISE 3');
-  // Change the value of the variable age to equal the age of the third person.
-
-  // --v-- write code here --v--
-
-  const age = people;
-
-  // --^-- write code here --^--
-
-  checkConditions([age, 10, 'age']);
-}
-
-{
-  console.log('EXERCISE 4');
-  // Change the value of the variable mothersName to equal the mothers name of the second person.
-
-  // --v-- write code here --v--
-
-  const mothersName = people;
-
-  // --^-- write code here --^--
-
-  checkConditions([mothersName, 'Jacqueline Bouvier', 'mothersName']);
-}
-
-{
-  console.log('EXERCISE 5');
-  // Change the value of the variable animaltype to equal the type of animal of Lisas pet.
-
-  // --v-- write code here --v--
-
-  const animaltype = people;
-
-  // --^-- write code here --^--
-
-  checkConditions([animaltype, 'dog', 'animaltype']);
-}
+export {
+  nameOfFirstPerson,
+  homersOtherFriends,
+  ageOfThirdPerson,
+  mothersNameOfSecondPerson,
+  animaltypeOfLisasPet,
+};
