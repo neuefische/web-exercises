612fc67793c58f71360822920294345fce3a03d8
PReichetanz <paul.reichetanz@gmx.de>
2022-08-31 15:28:00 +0200

    feat(js-forms-2): add readme for calculator challenge

--- sessions/js-forms-2/simple-live-calculator/js/index.js
+++ sessions/js-forms-2/simple-live-calculator/js/index.js
@@ -3,20 +3,3 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
-
-let firstValue = 0;
-let secondValue = 0;
-
-firstInput.addEventListener('input', event => {
-  firstValue = event.target.value;
-  calculateResult();
-});
-
-secondInput.addEventListener('input', event => {
-  secondValue = event.target.value;
-  calculateResult();
-});
-
-function calculateResult() {
-  result.textContent = 'Result: ' + firstValue * secondValue;
-}
