94cd18523029b68dc3b72b96b8e6237590de85e6
Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
2022-08-31 11:58:39 +0200

    feat(recap-project-2): character count demo (#73)
    * feat(recap-2): add character count demo
    
    * feat(recap-project-2): add review suggestions for demo

---
+++ sessions/recap-project-2/character-count-example/js/index.js
@@ -0,0 +1,15 @@
+console.clear();
+
+const questionElement = document.querySelector('[data-js="personalMessage"]');
+const amountLeft = document.querySelector('[data-js="amountLeft"]');
+const maxLength = questionElement.getAttribute('maxlength');
+
+const updateAmountLeft = value => {
+	amountLeft.innerText = value;
+};
+
+updateAmountLeft(maxLength);
+
+questionElement.addEventListener('input', () => {
+	updateAmountLeft(maxLength - questionElement.value.length);
+});
