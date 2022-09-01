0696782e2dcf9669e772f274ca6a49c13c53bfc3
Alex Claes <hello-github@aclaes.com>
2022-09-01 15:21:35 +0200

    feat(js-forms-2): add demo (#79)
    Co-authored-by: Alexander Claes <hello@aclaes.com>

---
+++ sessions/js-forms-2/demo-end/js/index.js
@@ -0,0 +1,49 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+const storage = document.querySelector('[data-js="storage"]');
+const passwordInput = document.querySelector('[data-js="password-input"]');
+
+function addStorageEntry(title, email, password) {
+	const entryElement = document.createElement('li');
+	entryElement.className = 'storage__entry';
+
+	entryElement.innerHTML = `
+		<h3	class="storage__title">${title}</h3>
+		<dl class="storage__credentials">
+			<dt>Email:</dt>
+			<dd>${email}</dd>
+			<dt>Password:</dt>
+			<dd>${password}</dd>
+		</dl>
+	`;
+
+	storage.append(entryElement);
+}
+
+function updatePasswordStrength(text) {
+	const percentage = Math.min(100, (text.length / 8) * 100);
+	const color = percentage < 100 ? 'var(--error)' : 'var(--success)';
+	passwordInput.style = `--strength: ${percentage}%; --color: ${color}`;
+}
+
+// ---v---v---v---v---v--- Event Handlers ---v---v---v---v---v---
+
+form.addEventListener('submit', event => {
+	event.preventDefault();
+
+	addStorageEntry(
+		event.target.elements.title.value,
+		event.target.elements.email.value,
+		event.target.elements.password.value,
+	);
+
+	event.target.reset();
+	event.target.elements.title.focus();
+
+	passwordInput.style = '';
+});
+
+passwordInput.addEventListener('input', event => {
+	updatePasswordStrength(event.target.value);
+});
