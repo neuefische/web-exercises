3b35e26204985df0ed849847ca7feb9cb3024332
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-09-02 09:48:16 +0200

    fix: remove unwanted challenges

--- sessions/js-forms-2/simple-live-calculator/js/index.js
+++
@@ -1,5 +0,0 @@
-console.clear();
-
-const firstInput = document.querySelector('[data-js="firstValue"]');
-const secondInput = document.querySelector('[data-js="secondValue"]');
-const result = document.querySelector('[data-js="result"]');
--- sessions/js-forms-2/simple-todolist/js/index.js
+++
@@ -1,4 +0,0 @@
-console.clear();
-
-const form = document.querySelector('[data-js="form"]');
-const todoList = document.querySelector('[data-js="todoList"]');
