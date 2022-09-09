38339162396415f3e358c181ee4686541574abde
PReichetanz <paul.reichetanz@gmx.de>
2022-09-09 09:09:58 +0200

    feat(js-structure-demo): add toggle functionality

--- sessions/js-structure/demo-start/index.js
+++ sessions/js-structure/demo-start/index.js
@@ -1,4 +1,4 @@
-function toggleBookmarks() {
+function toggleBookmark() {
 	const bookmarkButtons = document.querySelectorAll('[data-js="bookmark-button"]');
 
 	bookmarkButtons.forEach(button => {
@@ -8,4 +8,18 @@ function toggleBookmarks() {
 	});
 }
 
-toggleBookmarks();
+function toggleAnswer() {
+	const cardElements = document.querySelectorAll('[data-js="card"]');
+
+	cardElements.forEach(card => {
+		const answerButton = card.querySelector('[data-js="answer-button"]');
+		const answer = card.querySelector('[data-js="answer"]');
+
+		answerButton.addEventListener('click', () => {
+			answer.classList.toggle('card__answer--active');
+		});
+	});
+}
+
+toggleBookmark();
+toggleAnswer();
