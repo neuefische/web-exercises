3809f432740d0975c9c27329849df8d963f896f7
Jan Willem Henckel <farly.de@gmail.com>
2022-09-10 12:04:45 +0200

    fix: tests

--- sessions/js-structure/spacerocket/js/launchSequence.test.js
+++ sessions/js-structure/spacerocket/js/launchSequence.test.js
@@ -2,20 +2,53 @@ import {getNewRocket} from './core/rocket.js';
 import {FISHSAT, NFSAT} from './payload/satellites.js';
 import launchSequenceFunction from './launchSequence.js';
 
+beforeEach(() => {
+	global.window = {rocket: getNewRocket()};
+});
+
+afterEach(() => {
+	delete global.window;
+});
+
 test('The default export of launchSequence.js is a function', () => {
 	expect(typeof launchSequenceFunction).toBe('function');
 });
 
-test('launch() function correctly launches the rocket', () => {
-	global.window = {rocket: getNewRocket()};
-
+test('launch() function does not throw', () => {
 	expect(() => launchSequenceFunction()).not.toThrow();
+});
 
+test('rocket is fueled', () => {
+	launchSequenceFunction();
 	expect(window.rocket.fuel).toBe(true);
+});
+
+test('countdown is correct', () => {
+	launchSequenceFunction();
 	expect(window.rocket.countdown).toBe(5);
+});
+
+test('rocket did lift off', () => {
+	launchSequenceFunction();
 	expect(window.rocket.liftoff).toBe(true);
+});
+
+test('rocket contains NFSAT payload', () => {
+	launchSequenceFunction();
 	expect(window.rocket.payload).toContain(NFSAT);
+});
+
+test('rocket contains FISHSAT payload', () => {
+	launchSequenceFunction();
 	expect(window.rocket.payload).toContain(FISHSAT);
+});
+
+test('rocket has 2 payload items', () => {
+	launchSequenceFunction();
 	expect(window.rocket.payload.length).toBe(2);
+});
+
+test('rocket payload was deployed', () => {
+	launchSequenceFunction();
 	expect(window.rocket.payloadDeployed).toBe(true);
 });
