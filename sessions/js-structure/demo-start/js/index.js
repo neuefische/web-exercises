4e66d0323d9363687666b8e0b6733e819a202501
PReichetanz <paul.reichetanz@gmx.de>
2022-09-12 15:11:58 +0200

    feat(js-structure-demo): refactor demo start

--- sessions/js-structure/demo-start/js/index.js
+++ sessions/js-structure/demo-start/js/index.js
@@ -1,31 +1,52 @@
+/** Helper Functions */
+
+function getPlural(word) {
+	return word + 's';
+}
+
+let count = 0;
+function getAuthorName() {
+	const authors = ['Caesar', 'Horace', 'Homer', 'Ovid', 'Cicero'];
+
+	if (count > 4) {
+		count = 0;
+	}
+
+	const author = authors[count];
+	count++;
+	return author;
+}
+
 /** HEADER */
 const headerButton = document.querySelector('[data-js="header-button"]');
 const headline = document.querySelector('[data-js="headline"]');
 
 headerButton.addEventListener('click', () => {
-	headline.textContent = 'Classical Authors';
+	headline.textContent = `Classical ${getPlural('Author')}`;
 	headerButton.classList.add('header__button-clicked');
 });
 
 /** Card */
 
-const card = document.querySelector('[data-js="card"]');
-const cardButton = document.querySelector('[data-js="card-button"]');
+function changeCard(cardElement) {
+	const cardButton = cardElement.querySelector('[data-js="card-button"]');
 
-cardButton.addEventListener('click', () => {
-	card.children[0].textContent = 'Real Latin Placeholder Text';
-	card.children[1].textContent =
-		'Gallia est omnis divisa in partes tres, quarum unam incolunt Belgae, aliam Aquitani, tertiam qui ipsorum lingua Celtae, nostra Galli appellantur. (Caes. BG 1.1.1)';
-	cardButton.classList.add('card__button-clicked');
-	card.classList.add('card-changed');
-});
+	cardButton.addEventListener('click', () => {
+		cardElement.children[0].textContent = 'Real Latin Placeholder Text';
+		cardElement.children[1].textContent =
+			'Gallia est omnis divisa in partes tres, quarum unam incolunt Belgae, aliam Aquitani, tertiam qui ipsorum lingua Celtae, nostra Galli appellantur. (Caes. BG 1.1.1)';
+		cardButton.classList.add('card__button-clicked');
+		cardElement.classList.add('card-changed');
+	});
+}
+
+document.querySelectorAll('[data-js="card"]').forEach(card => changeCard(card));
 
 /** Classical Authors List */
 
 const addButton = document.querySelector('[data-js="add-button"]');
 const list = document.querySelector('[data-js="authors-list"]');
 
-let count = 0;
 addButton.addEventListener('click', () => {
 	if (!list.children.length) {
 		const headline = document.createElement('h2');
@@ -33,15 +54,9 @@ addButton.addEventListener('click', () => {
 		list.append(headline);
 	}
 
-	const authors = ['Caesar', 'Horace', 'Homer', 'Ovid', 'Cicero'];
-
-	if (count > 4) {
-		count = 0;
-	}
-
 	const listItem = document.createElement('li');
 	listItem.className = 'classical-authors__list-item';
-	listItem.textContent = authors[count];
+	listItem.textContent = getAuthorName();
 	count++;
 
 	list.append(listItem);
