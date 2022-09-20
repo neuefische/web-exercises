3399c0db501c6bad72e04b58b9948633dd607a38
Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
2022-09-20 16:14:28 +0200

    feat/js-structure-demo (#95)
    * feat(js-structure-demo): add start demo
    
    * feat(js-structure-demo): refactor demo start
    
    * fix(js-structure-demo): delete redundant count variable
    
    * feat(js-structure-demo): add missing function call
    
    * feat(js-structure-demo): add end version of demo
    
    * fix(js-structure-demo): missing file ending
    
    * fix(js-structure-demo): missing file ending
    
    * feat(js-structure-demo): refactor card to default export
    
    * fix: some demo fixes
    
    * feat: update demo to use component like structure independent of HTML
    
    * fix: remove type module from starter
    
    * fix: header function
    
    * fix: remove readmes
    
    Co-authored-by: Jan Willem Henckel <farly.de@gmail.com>

---
+++ sessions/js-structure/demo-start/index.js
@@ -0,0 +1,106 @@
+/** Authors */
+
+const authors = [
+	{
+		name: 'Homer',
+		text: 'Homer is the author of the Iliad and the Odyssey.',
+	},
+	{
+		name: 'Virgil',
+		text: 'Virgil is the author of the Aeneid.',
+	},
+	{
+		name: 'Ovid',
+		text: 'Ovid is the author of the Metamorphoses.',
+	},
+	{
+		name: 'Horace',
+		text: 'Horace is the author of the Odes.',
+	},
+	{
+		name: 'Caesar',
+		text: 'Caesar is the author of the Gallic Wars.',
+	},
+];
+
+/**
+ * Cipher
+ * https://en.wikipedia.org/wiki/Caesar_cipher
+ */
+
+const input = /*  */ 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const output = /* */ 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+
+function caesarCipher13(string) {
+	return string
+		.split('')
+		.map(character => {
+			// 💡 indexOf returns -1 if the character is not found
+			const index = input.indexOf(character);
+			if (index > -1) {
+				return output[index];
+			}
+
+			return character;
+		})
+		.join('');
+}
+
+/** Get root element */
+
+const root = document.querySelector('#root');
+
+/** Header */
+
+const headlineText = 'Classical Authors';
+
+const header = document.createElement('header');
+header.className = 'header';
+
+const headline = document.createElement('h1');
+headline.textContent = caesarCipher13(headlineText);
+
+const button = document.createElement('button');
+button.textContent = 'Decipher Headline';
+button.type = 'button';
+button.className = 'header__button';
+button.addEventListener('click', handleDecipherHeadlineButtonClick);
+
+header.append(headline, button);
+
+function handleDecipherHeadlineButtonClick() {
+	headline.textContent = headlineText;
+	button.setAttribute('disabled', '');
+}
+
+root.append(header);
+
+/** Main: Author Cards */
+
+authors.forEach(author => {
+	const card = document.createElement('article');
+	card.className = 'card';
+
+	const title = document.createElement('h2');
+	title.className = 'card__title';
+	title.textContent = author.name;
+
+	const text = document.createElement('p');
+	text.className = 'card__text';
+	text.textContent = caesarCipher13(author.text);
+
+	const button = document.createElement('button');
+	button.className = 'card__button';
+	button.textContent = 'Decipher';
+	button.addEventListener('click', handleDecipherButtonClick);
+
+	card.append(title, text, button);
+
+	function handleDecipherButtonClick() {
+		text.textContent = author.text;
+		button.setAttribute('disabled', '');
+		card.classList.add('card--deciphered');
+	}
+
+	root.append(card);
+});
