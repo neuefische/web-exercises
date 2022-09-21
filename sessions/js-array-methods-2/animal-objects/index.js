0ee14f3b96755f7a893bd0f6acdcc7a726ab20e0
Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
2022-09-21 15:37:55 +0200

    feat(array-methods-2): animal object challenge (#97)
    * feat(array-methods-2): add animal object challenge
    
    * fix(animal-objects-challenge): test for average weight
    
    use .toBeCloseTo() in order get rid of .toFixed() in solution
    
    * fix(animal-objects-challenge): backticks in readme
    
    * fix(array-methods-2): typos in animal objects readme

---
+++ sessions/js-array-methods-2/animal-objects/index.js
@@ -0,0 +1,93 @@
+console.clear();
+
+const animals = [
+	{
+		name: 'cat',
+		weight: 4,
+		continents: ['Europe', 'Asia', 'Africa', 'Australia', 'North-America', 'South-America'],
+	},
+	{
+		name: 'dog',
+		weight: 10,
+		continents: ['Europe', 'Asia', 'Africa', 'Australia', 'North-America', 'South-America'],
+	},
+	{name: 'elephant', weight: 4000, continents: ['Africa', 'Asia']},
+	{
+		name: 'rabbit',
+		weight: 2,
+		continents: ['Europe', 'Asia', 'Africa', 'Australia', 'North-America', 'South-America'],
+	},
+	{name: 'lion', weight: 200, continents: ['Africa']},
+	{name: 'tiger', weight: 150, continents: ['Asia']},
+	{
+		name: 'horse',
+		weight: 500,
+		continents: ['Europe', 'Asia', 'Africa', 'Australia', 'North-America', 'South-America'],
+	},
+	{name: 'giraffe', weight: 500, continents: ['Africa']},
+	{name: 'zebra', weight: 300, continents: ['Africa']},
+	{
+		name: 'penguin',
+		weight: 10,
+		continents: ['Africa', 'Australia', 'Antarctica', 'South-America'],
+	},
+	{
+		name: 'polar bear',
+		weight: 450,
+		continents: ['North-America', 'Asia', 'Arctic'],
+	},
+	{name: 'panda', weight: 120, continents: ['Asia']},
+	{name: 'koala', weight: 10, continents: ['Australia']},
+	{name: 'kangaroo', weight: 70, continents: ['Australia']},
+	{
+		name: 'monkey',
+		weight: 20,
+		continents: ['South-America', 'Asia', 'Africa'],
+	},
+	{name: 'sloth', weight: 8, continents: ['South-America']},
+	{name: 'hippo', weight: 1500, continents: ['Africa']},
+	{name: 'rhino', weight: 2000, continents: ['Asia', 'Africa']},
+];
+
+// Hint: Besides the array method, check out the string method `startsWith()`.
+const firstAnimalStartingWithLetterG = null;
+
+const indexOfAnimalWithNameLongerFive = null;
+
+// Note:
+// - Sorting strings is slightly more complicated than sorting numbers.
+// - You will need if-statements and return values of -1, 1, and 0.
+// - Check out MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+// Hint: There is no need to upper-/lowercase the strings before sorting them.
+// Hint: sort() mutates the original array, which is bad.
+// -> Use animals.slice().sort(...) to make a copy (and the tests work).
+
+const animalsSortedAlphabetically = null;
+
+const animalsSortedByWeightStartingWithLowest = null;
+
+const animalsSortedByWeightReversed = null;
+
+const animalWithWeightMoreThanFivehundredExists = null;
+
+// Hint: Filter for Europe first, then check every animal for its weight.
+const allAnimalsInEuropeWeighLessThanOnehundred = null;
+
+// Hint: filter + map + reduce
+const weightOfAllAnimalsInAfrica = null;
+
+// Hint: As above, but divided by the number of animals in Africa.
+const averageWeightOfAllAnimalsInAfrica = null;
+
+export {
+	firstAnimalStartingWithLetterG,
+	indexOfAnimalWithNameLongerFive,
+	animalsSortedAlphabetically,
+	animalsSortedByWeightStartingWithLowest,
+	animalsSortedByWeightReversed,
+	animalWithWeightMoreThanFivehundredExists,
+	allAnimalsInEuropeWeighLessThanOnehundred,
+	weightOfAllAnimalsInAfrica,
+	averageWeightOfAllAnimalsInAfrica,
+};
