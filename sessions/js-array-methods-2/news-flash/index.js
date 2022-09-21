6ce376fc1c05a24b8a250c929ac84724ce90b315
PReichetanz <paul.reichetanz@gmx.de>
2022-09-21 11:33:10 +0200

    feat(array-methods-2): add test functions for news flash

--- sessions/js-array-methods-2/news-flash/index.js
+++ sessions/js-array-methods-2/news-flash/index.js
@@ -1,5 +1,6 @@
 import {news} from './utils/news.js';
 import {Card} from './components/Card/Card.js';
+import {checkFilteredNews, checkSortedNews} from './utils/results.js';
 
 const container = document.querySelector('[data-js="card-container"]');
 
@@ -15,3 +16,8 @@ sortedNews.forEach(news => {
 	const cardElement = Card(news);
 	container.append(cardElement);
 });
+
+// Check your filter and sorting order here:
+checkFilteredNews(filteredNews);
+
+checkSortedNews(sortedNews);
