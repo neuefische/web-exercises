f6e1a1f0883839c2091cfffcbaf841fe94490975
Jan Willem Henckel <farly.de@gmail.com>
2022-09-22 09:35:54 +0200

    Update sessions/js-async-functions/api-status/js/index.js

--- sessions/js-async-functions/api-status/js/index.js
+++ sessions/js-async-functions/api-status/js/index.js
@@ -3,10 +3,23 @@ const button = document.querySelector('[data-js="status-button"]');
 
 const api = 'https://example-apis.vercel.app/api/status';
 
-async function checkIfAPIIsAvailable() {
-	const response = await fetch(api);
+// something needs to change here…
+// ↙️
+function checkIfApiIsAvailable() {
+    /**
+	 * Hint 1:
+	 * Use the `fetch()` function and pass the `apiStatusUrl` into it
+	 *
+	 * Hint 2:
+	 * The fetch function returns a promise which resolves to a Response
+	 * object once it is ready.
+	 **/
+
+	// --v-- write your code here --v--
+
+	// --^-- write your code here --^--
 }
 
 button.addEventListener('click', () => {
-	checkIfAPIIsAvailable();
+	checkIfApiIsAvailable();
 });
