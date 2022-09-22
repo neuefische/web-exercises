6f02e5b5a8092d6dd0dbae525ef379bf4c793292
Jan Willem Henckel <farly.de@gmail.com>
2022-09-22 11:40:56 +0200

    fix: implement review

--- sessions/js-async-functions/relay-race/js/index.js
+++ sessions/js-async-functions/relay-race/js/index.js
@@ -11,22 +11,24 @@ const runner3 = document.querySelector('[data-js="runner"]:nth-of-type(3)');
 const startRaceButton = document.querySelector('[data-js="start-race-button"]');
 
 // You'll need to change something about this callback function:
-//                                        |
-//                                        v
+//                                         ↙️
 startRaceButton.addEventListener('click', () => {
 	startRaceButton.setAttribute('disabled', '');
 
 	/**
 	 * Hint 1:
-	 * Use `.animate(runnerKeyframes, runnerAnimation)` to animate the runners
-	 *
-	 * Hint 2:
-	 * The animate function returns an animation object that has a `finished`
-	 * property that is a promise which resolves when the animation is done.
-	 *
-	 * Hint 3:
 	 * The relay runners are already available in the global scope as `runner1`,
 	 * `runner2` and `runner3`.
+	 *
+	 * Hint 2:
+	 * Use `.animate(runnerKeyframes, runnerAnimation)` on the runner elements
+	 * to animate the runners.
+	 * You may need to store the result of this function call inside a variable
+	 * to access its properties.
+	 *
+	 * Hint 3:
+	 * The animate function returns an animation object that has a `finished`
+	 * property that is a promise which resolves when the animation is done.
 	 **/
 
 	// --v-- write your code here --v--
