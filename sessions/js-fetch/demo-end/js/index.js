07595c1bbd0f538630bc26e0b5326ba976d4dd33
Ceren Jana Akkuec <cjakkuec@gmail.com>
2022-09-23 15:13:46 +0200

    feat: created js-fetch session folder, added demo start + end

---
+++ sessions/js-fetch/demo-end/js/index.js
@@ -0,0 +1,40 @@
+// Import Functions & Modules
+import {JokeSection} from '../components/JokeSection/JokeSection.js';
+import {Joke} from '../components/Joke/Joke.js';
+import {getRandomInt} from '../utils/getRandomInt.js';
+
+// Declare root Element
+const root = document.body;
+
+// Assemble DOM
+const jokeSection = JokeSection();
+const joke = Joke();
+
+root.append(jokeSection);
+jokeSection.append(joke);
+
+// Get a random joke from a Bad Jokes API
+async function getJoke() {
+	try {
+		const response = await fetch('https://example-apis.vercel.app/api/bad-jokes');
+
+		// Failure (Bad response)
+		if (!response.ok) {
+			console.error('Bad Response');
+		} else {
+			// Success
+			const jokeData = await response.json();
+
+			const randomIndex = getRandomInt(0, jokeData.length);
+
+			const randomJoke = jokeData[randomIndex];
+
+			joke.append(randomJoke.joke);
+		}
+		// Failure (Error during fetch)
+	} catch (error) {
+		console.error('An Error occurred');
+	}
+}
+
+getJoke();
