d3fdd6fe33ca27de5b72577079d94784115c2465
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-10-10 10:12:26 +0200

    chore: migrate challenge text to README.md for named vs anonymous

--- sessions/js-callback-functions/user-login-callback-1-named-vs-anonymous/js/index.js
+++ sessions/js-callback-functions/user-login-callback-1-named-vs-anonymous/js/index.js
@@ -4,18 +4,4 @@ function handleUserLogin(onSuccess) {
 	onSuccess();
 }
 
-// The exercise starts here!
-
-// You should write a callback function in two different ways.
-// In both cases your callback function should log the following text to the console:
-// "Welcome! You are logged in now."
-
-// 1.
-// Write a function with the "function" keyword named "showWelcomeMessage"
-// Call "handleUserLogin" and pass your callback function as the first argument.
-// (You should see the log from your callback message)
-
-// 2.
-// Write an anonymous function with the fat arrow syntax.
-// Call "handleUserLogin" and pass your callback function as the first argument.
-// (You should see the log from your callback message)
+// Call handleUserLogin below!
