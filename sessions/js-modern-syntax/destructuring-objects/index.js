85f49f28e9c560476da97fb9bb6340b09f9e18ac
Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
2022-10-13 16:23:48 +0200

    JS Modern Syntax - Destructuring Objects Challenge (#113)
    * feat: add boilerplate for 4 exercises
    
    * feat: add destructuring objects content
    
    * feat: add array destructuring content
    
    * feat: add async-await-ghibli content
    
    * feat: add async-await-star-wars content
    
    * refactored es-next session and added a check function instead of simple console logs to control the solutions of the students
    
    * refactored es-next session and added a check function instead of simple console logs to control the solutions of the students
    
    * changed the error message and console.log to console.error in checkConditions function
    
    * reworked es-next array exercises
    
    * reworked es-next object exercises and added a readme for each of the four exercises
    
    * add exercise 6 array destructuring and fixed typos
    
    * fixed typo
    
    * fix wrong variable name
    
    * fix wrong variable name
    
    * feat(js-modern-syntax): delete old challenges
    
    * feat(js-modern-syntax): delete array challenge
    
    * feat(js-modern-syntax): update object destructuring challenge
    
    * fix(js-modern-syntax): export of variables in object challenge
    
    * fix(js-modern-syntax): variable name in object challenge test file
    
    * Update sessions/js-modern-syntax/destructuring-objects/index.js
    
    Co-authored-by: Andrea Hohnsen <94636438+ahohnsen@users.noreply.github.com>
    
    * Update sessions/js-modern-syntax/destructuring-objects/index.js
    
    Co-authored-by: Andrea Hohnsen <94636438+ahohnsen@users.noreply.github.com>
    
    * Update sessions/js-modern-syntax/destructuring-objects/index.js
    
    Co-authored-by: Andrea Hohnsen <94636438+ahohnsen@users.noreply.github.com>
    
    * Update sessions/js-modern-syntax/destructuring-objects/index.test.js
    
    Co-authored-by: Andrea Hohnsen <94636438+ahohnsen@users.noreply.github.com>
    
    Co-authored-by: Felix Kirchhoff <felix.kirchhoff@neuefische.de>
    Co-authored-by: Stefan Korder <stefan.korder@neuefische.de>
    Co-authored-by: stefankorder <77627144+stefankorder@users.noreply.github.com>
    Co-authored-by: CJAkkuec <90191489+CJAkkuec@users.noreply.github.com>
    Co-authored-by: Andrea Hohnsen <94636438+ahohnsen@users.noreply.github.com>

---
+++ sessions/js-modern-syntax/destructuring-objects/index.js
@@ -0,0 +1,42 @@
+console.clear();
+
+// EXERCISE 1
+// Extract only the course name from the company object.
+// Make sure to export your variable as in the example below to make the tests work.
+
+const company = {
+	name: 'neuefische',
+	course: 'Web Development',
+	location: 'Hamburg',
+};
+
+// example: export const {value1} = myObject;
+
+// EXERCISE 2
+// We have an object called 'user'.
+// Write the destructuring assignment that reads:
+//  2.1: 'name' property into the variable 'name'.
+//  2.2: 'years' property into the variable 'age'.
+//  2.3: 'isAdmin' property into the variable 'isAdmin' (false, if no such property exists)
+
+const user = {name: 'John', years: 30};
+
+// EXERCISE 3
+// Extract the variables from the dog object and
+// rename them like "dogName", "dogBreed", and "dogAge", respectively.
+
+const dog = {
+	name: 'Pluto',
+	breed: 'husky',
+	age: 5,
+};
+
+// EXERCISE 4
+// Extract the 'lastName' property from the person object as "personLastName".
+// Store all other properties in an object called "moreInformation".
+
+const person = {
+	lastName: 'Main',
+	age: 24,
+	firstName: 'Alex',
+};
