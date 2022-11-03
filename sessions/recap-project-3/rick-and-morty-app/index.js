15c538a76990e364226a7b700d3504f3a456e697
Jan Willem Henckel <farly.de@gmail.com>
2022-11-03 12:26:12 +0100

    format using prettier

--- sessions/recap-project-3/rick-and-morty-app/index.js
+++ sessions/recap-project-3/rick-and-morty-app/index.js
@@ -1,5 +1,7 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
+const searchBarContainer = document.querySelector(
+  '[data-js="search-bar-container"]'
+);
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
@@ -9,4 +11,4 @@ const pagination = document.querySelector('[data-js="pagination"]');
 // States
 const maxPage = 1;
 const page = 1;
-const searchQuery = '';
+const searchQuery = "";
