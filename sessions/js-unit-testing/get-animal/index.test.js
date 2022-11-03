f17de662ef7186017fd3c4480fff0814d5b449f5
PReichetanz <paul.reichetanz@gmx.de>
2022-11-03 12:45:08 +0100

    update get animal challenge according to review
    - run new prettier config
    - fix wording and typos

--- sessions/js-unit-testing/get-animal/index.test.js
+++ sessions/js-unit-testing/get-animal/index.test.js
@@ -1,14 +1,14 @@
-import {getAnimal} from './index.js';
+import { getAnimal } from "./index.js";
 
-test('getAnimals() returns a string "I really like cats!" if called with the argument "cat".', () => {
-	const result = getAnimal('cat');
-	expect(result).toBe('I really like cats!');
+test("getAnimals() returns a string 'I really like cats!' if called with the argument 'cats'.", () => {
+  const result = getAnimal("cats");
+  expect(result).toBe("I totally love cats!");
 });
-test('getAnimals() returns a string "I like [animalPluralName]!" if called with a string argument.', () => {
-	const result = getAnimal('dog');
-	expect(result).toBe('I like dogs!');
+test("getAnimals() returns a string 'I like [animalPluralName]!' if called with a string argument.", () => {
+  const result = getAnimal("dogs");
+  expect(result).toBe("I like dogs!");
 });
-test('getAnimals() returns a string "I do not like animals at all!" if called without arguments.', () => {
-	const result = getAnimal();
-	expect(result).toBe('I do not like animals at all!');
+test("getAnimals() returns a string 'I do not like animals at all!' if called without arguments.", () => {
+  const result = getAnimal();
+  expect(result).toBe("I do not like animals at all!");
 });
