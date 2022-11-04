0026b2bd6e79aa6f8979252977e84a68d5e5a838
Jan Willem Henckel <farly.de@gmail.com>
2022-11-04 17:56:35 +0100

    add get script to make pr generation easier

---
+++ scripts/get.js
@@ -0,0 +1,87 @@
+import branchName from "branch-name";
+import clipboard from "clipboardy";
+import fs from "fs-extra";
+import fuzzy from "fuzzy";
+import { globby } from "globby";
+import inquirer from "inquirer";
+import inquirerPrompt from "inquirer-autocomplete-prompt";
+
+inquirer.registerPrompt("autocomplete", inquirerPrompt);
+
+function getPrMarkdown({ sessionName, exerciseName, branchName }) {
+  const path = `${sessionName}/${exerciseName}`;
+
+  // get the challenge template from ./get/challenge-template.md
+  const challengeMd = fs
+    .readFileSync("./scripts/get/challenge-template.md", "utf8")
+    .replace(/PATH/g, path);
+
+  // get the pr template from ./get/pr-template.md
+  const prMd = fs
+    .readFileSync("./scripts/get/pr-template.md", "utf8")
+    .replace(/PATH/g, path)
+    .replace(/BRANCH_NAME/g, branchName)
+    .replace(/CHALLENGE_MARKDOWN/g, "````md\n" + challengeMd + "````");
+
+  return prMd;
+}
+
+try {
+  const exercises = await globby("*/*", {
+    onlyDirectories: true,
+    expandDirectories: false,
+    cwd: "./sessions",
+  });
+
+  // split into sessions
+  const sessionsObj = exercises.reduce((sessions, exercise) => {
+    const [session, name] = exercise.split("/");
+    sessions[session] = sessions[session] || [];
+    sessions[session].push(name);
+    return sessions;
+  }, {});
+
+  const sessions = Object.keys(sessionsObj);
+
+  const currentBranch = await branchName.get();
+
+  const input = await inquirer.prompt([
+    {
+      type: "autocomplete",
+      name: "sessionName",
+      message: "Session name",
+      async source(answers, input = "") {
+        return fuzzy.filter(input, sessions).map((el) => el.original);
+      },
+    },
+    {
+      type: "autocomplete",
+      name: "exerciseName",
+      message: "Exercise name",
+      when: (answers) => sessionsObj[answers.sessionName]?.length > 1,
+      async source(answers, input = "") {
+        return fuzzy
+          .filter(input, sessionsObj[answers.sessionName])
+          .map((el) => el.original);
+      },
+    },
+  ]);
+
+  const sessionName = input.sessionName;
+  const exerciseName = input.exerciseName || sessionsObj[sessionName][0];
+
+  const md = getPrMarkdown({
+    sessionName,
+    exerciseName,
+    branchName: currentBranch,
+  });
+
+  clipboard.writeSync(md);
+
+  console.log("PR Markdown copied to clipboard!");
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
+
+process.exit(0);
---
+++ scripts/get/challenge-template.md
@@ -0,0 +1,7 @@
+Solve this challenge on
+[🔗 **CodeSandbox**](https://codesandbox.io/s/github/neuefische/web-exercises/tree/main/sessions/PATH?file=/README.md)
+or locally by running this command in your Terminal:
+
+```
+npx ghcd@latest neuefische/web-exercises/tree/main/sessions/PATH -i
+```
