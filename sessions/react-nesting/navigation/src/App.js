4bb803803673a7624e2a62707d03a0b9de332a16
PReichetanz <paul.reichetanz@gmx.de>
2022-11-08 14:55:16 +0100

    feat(react-nesting): add navigation challenge

---
+++ sessions/react-nesting/navigation/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./styles.css";
+
+import avatar from "./img/avatar.jpg";
+import logo from "./img/logo.jpg";
+
+export default function App() {
+  return (
+    <header className="header">
+      <a href="#">
+        <img className="round-image" src={logo} alt="logo" />
+      </a>
+      <nav>
+        <a className="navigation__link" href="#home">
+          Home
+        </a>
+        <a className="navigation__link" href="#about">
+          About
+        </a>
+        <a className="navigation__link" href="#impressum">
+          Impressum
+        </a>
+      </nav>
+      <button
+        type="button"
+        onClick={() => console.log("I could toggle a profile!")}
+        aria-label="toggle profile"
+      >
+        <img className="round-image" src={avatar} alt="avatar image" />
+      </button>
+    </header>
+  );
+}
