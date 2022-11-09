67d11df82bcb12211cdaf1f814c82513ed90b7fb
Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
2022-11-09 11:34:32 +0100

    React Nesting: Navigation Challenge (#144)
    * feat(react-nesting): add navigation challenge
    
    * Update sessions/react-nesting/navigation/src/App.js
    
    Co-authored-by: Jan Willem Henckel <farly.de@gmail.com>
    
    * Update sessions/react-nesting/navigation/src/styles.css
    
    Co-authored-by: Jan Willem Henckel <farly.de@gmail.com>
    
    * Update sessions/react-nesting/navigation/README.md
    
    Co-authored-by: Jan Willem Henckel <farly.de@gmail.com>
    
    * Update sessions/react-nesting/navigation/src/App.js
    
    Co-authored-by: Jan Willem Henckel <farly.de@gmail.com>
    
    * Update sessions/react-nesting/navigation/README.md
    
    Co-authored-by: Jan Willem Henckel <farly.de@gmail.com>
    
    * Update sessions/react-nesting/navigation/README.md
    
    Co-authored-by: Jan Willem Henckel <farly.de@gmail.com>
    
    * Update sessions/react-nesting/navigation/README.md
    
    Co-authored-by: Jan Willem Henckel <farly.de@gmail.com>
    
    * Update sessions/react-nesting/navigation/README.md
    
    Co-authored-by: Jan Willem Henckel <farly.de@gmail.com>

---
+++ sessions/react-nesting/navigation/src/App.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+
+import avatar from "./img/avatar.jpg";
+import logo from "./img/logo.jpg";
+
+export default function App() {
+  return (
+    <>
+      <header className="header">
+        <a href="#">
+          <img className="round-image" src={logo} alt="logo" />
+        </a>
+        <nav>
+          <a className="navigation__link" href="#home">
+            Home
+          </a>
+          <a className="navigation__link" href="#about">
+            About
+          </a>
+          <a className="navigation__link" href="#impressum">
+            Impressum
+          </a>
+        </nav>
+        <button
+          type="button"
+          onClick={() => console.log("I could toggle a profile!")}
+          aria-label="toggle profile"
+        >
+          <img className="round-image" src={avatar} alt="avatar" />
+        </button>
+      </header>
+      <main>content goes here…</main>
+    </>
+  );
+}
