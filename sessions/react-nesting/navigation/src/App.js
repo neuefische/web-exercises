f357e88fea15b7786a7f2edccd7839c7b58f5364
Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
2022-11-09 11:24:27 +0100

    Update sessions/react-nesting/navigation/src/App.js
    Co-authored-by: Jan Willem Henckel <farly.de@gmail.com>

--- sessions/react-nesting/navigation/src/App.js
+++ sessions/react-nesting/navigation/src/App.js
@@ -6,28 +6,31 @@ import logo from "./img/logo.jpg";
 
 export default function App() {
   return (
-    <header className="header">
-      <a href="#">
-        <img className="round-image" src={logo} alt="logo" />
-      </a>
-      <nav>
-        <a className="navigation__link" href="#home">
-          Home
+    <>
+      <header className="header">
+        <a href="#">
+          <img className="round-image" src={logo} alt="logo" />
         </a>
-        <a className="navigation__link" href="#about">
-          About
-        </a>
-        <a className="navigation__link" href="#impressum">
-          Impressum
-        </a>
-      </nav>
-      <button
-        type="button"
-        onClick={() => console.log("I could toggle a profile!")}
-        aria-label="toggle profile"
-      >
-        <img className="round-image" src={avatar} alt="avatar image" />
-      </button>
-    </header>
+        <nav>
+          <a className="navigation__link" href="#home">
+            Home
+          </a>
+          <a className="navigation__link" href="#about">
+            About
+          </a>
+          <a className="navigation__link" href="#impressum">
+            Impressum
+          </a>
+        </nav>
+        <button
+          type="button"
+          onClick={() => console.log("I could toggle a profile!")}
+          aria-label="toggle profile"
+        >
+          <img className="round-image" src={avatar} alt="avatar" />
+        </button>
+      </header>
+      <main>content goes here…</main>
+    </>
   );
 }
