eb8850fd4dcf6aef45406eb40aa83d0c4384975d
PReichetanz <paul.reichetanz@gmx.de>
2022-11-14 10:46:43 +0100

    feat(react-state-2): add colored number challenge

---
+++ sessions/react-state-2/colored-number/src/components/Counter.js
@@ -0,0 +1,26 @@
+import ColoredNumber from "./ColoredNumber";
+
+export default function Counter() {
+  return (
+    <div className="counter">
+      <h1>Colored Number</h1>
+      <ColoredNumber value={0} />
+      <div className="counter__buttons-container">
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="increment count"
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="decrement count"
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
