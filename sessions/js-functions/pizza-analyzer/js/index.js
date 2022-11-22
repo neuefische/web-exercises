2a857e41761ff66afe0a899777b9bd11de8864ae
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-11-22 11:21:17 +0100

    feat: add color change for output section

--- sessions/js-functions/pizza-analyzer/js/index.js
+++ sessions/js-functions/pizza-analyzer/js/index.js
@@ -4,22 +4,24 @@ const pizzaInput1 = document.querySelector('[data-js="pizza-input-1"]');
 const pizza1 = document.querySelector('[data-js="pizza-1"]');
 const pizzaInput2 = document.querySelector('[data-js="pizza-input-2"]');
 const pizza2 = document.querySelector('[data-js="pizza-2"]');
+const outputSection = document.querySelector('[data-js="output-section"]');
+
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
   const pizzaSize1 = pizzaInput1.value;
   const pizzaSize2 = pizzaInput2.value;
   calculatePizzaGain(pizzaSize1, pizzaSize2);
-
   updatePizzaDisplay(pizza1, pizzaSize1);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
   const pizzaSize1 = pizzaInput1.value;
   const pizzaSize2 = pizzaInput2.value;
   calculatePizzaGain(pizzaSize1, pizzaSize2);
-
   updatePizzaDisplay(pizza2, pizzaSize2);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 function calculatePizzaGain(diameter1, diameter2) {
@@ -31,4 +33,12 @@ function calculatePizzaGain(diameter1, diameter2) {
 
 function updatePizzaDisplay(pizzaElement, newSize) {
   pizzaElement.style.width = (newSize / 24) * 100 + "px";
+}
+
+function updateOutputColor(size1, size2) {
+  if (size1 > size2) {
+    outputSection.style.background = "var(--red)";
+  } else {
+    outputSection.style.background = "var(--green)";
+  }
 }
