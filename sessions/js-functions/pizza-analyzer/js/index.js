78a7c071df0a2230de70c0d3cfa2122dbdf18c00
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-11-22 11:16:27 +0100

    feat: add basic solution

---
+++ sessions/js-functions/pizza-analyzer/js/index.js
@@ -0,0 +1,34 @@
+console.clear();
+
+const pizzaInput1 = document.querySelector('[data-js="pizza-input-1"]');
+const pizza1 = document.querySelector('[data-js="pizza-1"]');
+const pizzaInput2 = document.querySelector('[data-js="pizza-input-2"]');
+const pizza2 = document.querySelector('[data-js="pizza-2"]');
+const output = document.querySelector('[data-js="output"]');
+
+pizzaInput1.addEventListener("input", () => {
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+
+  updatePizzaDisplay(pizza1, pizzaSize1);
+});
+
+pizzaInput2.addEventListener("input", () => {
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+
+  updatePizzaDisplay(pizza2, pizzaSize2);
+});
+
+function calculatePizzaGain(diameter1, diameter2) {
+  const area1 = Math.PI * (diameter1 / 2) ** 2;
+  const area2 = Math.PI * (diameter2 / 2) ** 2;
+  const gain = ((area2 - area1) / area1) * 100;
+  output.textContent = gain.toFixed(0);
+}
+
+function updatePizzaDisplay(pizzaElement, newSize) {
+  pizzaElement.style.width = (newSize / 24) * 100 + "px";
+}
