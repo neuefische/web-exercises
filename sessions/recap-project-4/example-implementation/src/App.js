f570a63ef04e1d039647fe6a439940c7b6baec5e
PReichetanz <paul.reichetanz@gmx.de>
2022-11-23 16:54:44 +0100

    add first version of fetch (without working api)

--- sessions/recap-project-4/example-implementation/src/App.js
+++ sessions/recap-project-4/example-implementation/src/App.js
@@ -1,4 +1,5 @@
 import useLocalStorageState from "use-local-storage-state";
+import { useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form/index.js";
 import List from "./components/List/index.js";
@@ -8,11 +9,30 @@ function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
+  const [weather, setWeather] = useLocalStorageState("weather", {
+    defaultValue: null,
+  });
+  console.log(weather);
 
-  const isGoodWeather = true;
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        const response = await fetch("api to implement");
+        if (response.ok) {
+          setWeather({ temperatur: 13, condition: "🌞", isGoodWeather: true });
+        } else {
+          console.log("Something went wrong!");
+          setWeather({ temperatur: 13, condition: "🌧️", isGoodWeather: false });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchWeather();
+  }, []);
 
   const filteredActivities = activities.filter(
-    (activity) => activity.isGoodWeather === isGoodWeather
+    (activity) => activity.isGoodWeather === weather.isGoodWeather
   );
 
   function handleAddActivity(newActivity) {
@@ -25,9 +45,13 @@ function App() {
 
   return (
     <div className="App">
+      <h1 className="App__heading">
+        <span>{weather.condition}</span>
+        <span>{weather.temperatur}</span>
+      </h1>
       <List
         activities={filteredActivities}
-        isGoodWeather={isGoodWeather}
+        isGoodWeather={weather.isGoodWeather}
         onDeleteActivity={handleDeleteActivity}
       />
       <Form onAddActivity={handleAddActivity} />
