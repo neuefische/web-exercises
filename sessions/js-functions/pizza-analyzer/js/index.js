34566bb1baf3ea1ad65cc7d1d73f98cbb45f0d87
Felix Kirchhoff <59687330+F-Kirchhoff@users.noreply.github.com>
2022-11-23 17:09:49 +0100

    Js functions pizza analyzer (#185)
    * feat: add basic solution
    
    * feat: add color change for output section
    
    * chore: add challenge task descriptions
    
    * fix: remove solution code
    
    * feat: improve pizza display style
    
    * fix: change pizza border size
    
    * feat: add comments where to start coding
    
    * fix: typo
    
    * Apply suggestions from code review
    
    Co-authored-by: Sheb <michael@sheb.rocks>
    
    * fix: include suggestions from review
    
    * fix: remove outdated rulesest
    
    * Apply suggestions from code review
    
    Co-authored-by: Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
    
    * fix: choose better name for output display element
    
    * feat: add Math.round() to the task description
    
    * chore: add additional hint to exercise description
    
    Co-authored-by: Felix Kirchhoff <felix.kirchhoff@neuefische.de>
    Co-authored-by: Sheb <michael@sheb.rocks>
    Co-authored-by: Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>

---
+++ sessions/js-functions/pizza-analyzer/js/index.js
@@ -0,0 +1,35 @@
+console.clear();
+
+const pizzaInput1 = document.querySelector('[data-js="pizza-input-1"]');
+const pizza1 = document.querySelector('[data-js="pizza-1"]');
+const pizzaInput2 = document.querySelector('[data-js="pizza-input-2"]');
+const pizza2 = document.querySelector('[data-js="pizza-2"]');
+const outputSection = document.querySelector('[data-js="output-section"]');
+
+const output = document.querySelector('[data-js="output"]');
+
+pizzaInput1.addEventListener("input", () => {
+  // write your code here
+});
+
+pizzaInput2.addEventListener("input", () => {
+  // write your code here
+});
+
+// Task 1
+
+function calculatePizzaGain(diameter1, diameter2) {
+  // write your code here
+}
+
+// Task 2
+
+function updatePizzaDisplay(pizzaElement, newSize) {
+  // write your code here
+}
+
+// Task 3
+
+function updateOutputColor(size1, size2) {
+  // write your code here
+}
