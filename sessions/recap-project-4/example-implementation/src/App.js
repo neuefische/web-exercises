6dad6677338aaf3ab195e2b0a826edd9db58ef70
PReichetanz <paul.reichetanz@gmx.de>
2022-11-23 15:44:01 +0100

    fix wrong import

--- sessions/recap-project-4/example-implementation/src/App.js
+++ sessions/recap-project-4/example-implementation/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Form from "./components/Form/index.js";
 import List from "./components/List/index.js";
 import { nanoid } from "nanoid";
-import { filter } from "fuzzy";
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
