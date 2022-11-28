a417e57a7489afc20161a40a79204a26efacf3a4
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-11-28 16:07:14 +0100

    fix typos for arrays and objects exercises

--- sessions/js-objects-and-arrays/arrays/index.test.js
+++ sessions/js-objects-and-arrays/arrays/index.test.js
@@ -10,44 +10,44 @@ import {
   shiftedFruits,
 } from "./index";
 
-test("Exersice 1: Array exampleArray contains a number and a string", () => {
+test("Exercise 1: Array exampleArray contains a number and a string", () => {
   expect(exampleArray).toEqual(
     expect.arrayContaining([expect.any(String), expect.any(Number)])
   );
 });
 
-test("Exersice 2: Array nestedArray contains an array", () => {
+test("Exercise 2: Array nestedArray contains an array", () => {
   expect(nestedArray).toEqual(expect.arrayContaining([expect.any(Array)]));
 });
 
-test("Exersice 3: firstNumber is 20", () => {
+test("Exercise 3: firstNumber is 20", () => {
   expect(firstNumber).toEqual(20);
 });
 
-test("Exersice 4: Array fruits first Element is mango", () => {
+test("Exercise 4: Array fruits first Element is mango", () => {
   expect(fruits[0]).toEqual("mango");
 });
 
-test("Exersice 5: nestedNumber is 40", () => {
+test("Exercise 5: nestedNumber is 40", () => {
   expect(nestedNumber).toEqual(40);
 });
 
-test("Exersice 6: Array petsWithPush fourth element is rat and the length is 4", () => {
+test("Exercise 6: Array petsWithPush fourth element is rat and the length is 4", () => {
   expect(petsWithPush).toEqual(expect.arrayContaining(["rat"]));
   expect(petsWithPush).toHaveLength(4);
 });
 
-test("Exersice 7: Array fruitsWithPop contains no element with name mango and the length is 2", () => {
+test("Exercise 7: Array fruitsWithPop contains no element with name mango and the length is 2", () => {
   expect(fruitsWithPop).not.toEqual(expect.arrayContaining(["mango"]));
   expect(fruitsWithPop).toHaveLength(2);
 });
 
-test("Exersice 8: Array unshiftedPets first element is hamster and the length is 4", () => {
+test("Exercise 8: Array unshiftedPets first element is hamster and the length is 4", () => {
   expect(unshiftedPets).toEqual(expect.arrayContaining(["hamster"]));
   expect(unshiftedPets).toHaveLength(4);
 });
 
-test("Exersice 9: Array shiftedFruits contains no element with name apple and the length is 2", () => {
+test("Exercise 9: Array shiftedFruits contains no element with name apple and the length is 2", () => {
   expect(shiftedFruits).not.toEqual(expect.arrayContaining(["apple"]));
   expect(shiftedFruits).toHaveLength(2);
 });
