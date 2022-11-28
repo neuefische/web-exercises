67663b96473ed9926264686d0abea0870627f771
Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
2022-11-28 15:44:02 +0100

    Update sessions/recap-project-4/example-implementation/src/components/List/index.js
    Co-authored-by: Alex Claes <hello-github@aclaes.com>

--- sessions/recap-project-4/example-implementation/src/components/List/index.js
+++ sessions/recap-project-4/example-implementation/src/components/List/index.js
@@ -4,9 +4,7 @@ export default function List({ activities, isGoodWeather, onDeleteActivity }) {
   return (
     <>
       <h2>
-        {isGoodWeather === null
-          ? null
-          : isGoodWeather
+       {isGoodWeather
           ? "The weather is awesome! Go outside and:"
           : "Bad weather outside! Here's what you can do now:"}
       </h2>
