57bcd2a224ffe75f145389ce5237ddb3e0c00e7e
Jan Willem Henckel <farly.de@gmail.com>
2022-11-30 18:41:46 +0100

    add @next/font

--- sessions/react-styled-components/lotr-app-styling/styles.js
+++ sessions/react-styled-components/lotr-app-styling/styles.js
@@ -1,37 +1,45 @@
 import { createGlobalStyle } from "styled-components";
+import { Lora } from "@next/font/google";
+
+const lora = Lora({
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+  variable: "--font-family",
+});
 
 export const GlobalStyle = createGlobalStyle`
-:root {
-  /* Color styles */
-  --color-earth: #282828;
-  --color-clouds: #f1f1f1;
-  --color-smoke: #d4d1cd;
+  :root {
+    /* Color styles */
+    --color-earth: #282828;
+    --color-clouds: #f1f1f1;
+    --color-smoke: #d4d1cd;
 
-  /* Font styles */
-  /*
-    use like:
-    font: var(--font-body);
-  */
-  --font-headline-1: normal 700 44px/48px "Lora", serif;
-  --font-headline-2: normal 700 32px/41px "Lora", serif;
-  --font-title: normal 700 16px/20px "Lora", serif;
-  --font-caption: normal 500 12px/15px "Lora", serif;
-  --font-caption--italic: italic 400 12px/15px "Lora", serif;
-  --font-body: normal 400 16px/20px "Lora", serif;
+    /* Font styles */
+    /*
+      use like:
+      font: var(--font-body);
+    */
+    --font-headline-1: normal 700 44px/48px ${lora.style.fontFamily};
+    --font-headline-2: normal 700 32px/41px ${lora.style.fontFamily};
+    --font-title: normal 700 16px/20px ${lora.style.fontFamily};
+    --font-caption: normal 500 12px/15px ${lora.style.fontFamily};
+    --font-caption--italic: italic 400 12px/15px ${lora.style.fontFamily};
+    --font-body: normal 400 16px/20px ${lora.style.fontFamily};
 
-  /* Effect styles */
-  /*
-    use like:
-    box-shadow: var(--box-shadow-book);
-  */
-  --box-shadow-book: 0 4px 8px -2px rgba(0, 0, 0, 0.09),
-    0 7px 4px -4px rgba(0, 0, 0, 0.07), 0 16px 8px -8px rgba(0, 0, 0, 0.07),
-    0 22px 12px -12px rgba(0, 0, 0, 0.07), 0 2px 10px 0 rgba(0, 0, 0, 0.06);
+    /* Effect styles */
+    /*
+      use like:
+      box-shadow: var(--box-shadow-book);
+    */
+    --box-shadow-book: 0 4px 8px -2px rgba(0, 0, 0, 0.09),
+      0 7px 4px -4px rgba(0, 0, 0, 0.07), 0 16px 8px -8px rgba(0, 0, 0, 0.07),
+      0 22px 12px -12px rgba(0, 0, 0, 0.07), 0 2px 10px 0 rgba(0, 0, 0, 0.06);
 
-  --box-shadow-book--hover: 0 6px 11px -2px rgba(0, 0, 0, 0.12),
-    0 8px 7px -4px rgba(0, 0, 0, 0.09), 0 17px 11px -8px rgba(0, 0, 0, 0.09),
-    0 24px 15px -12px rgba(0, 0, 0, 0.09), 0 4px 15px 0 rgba(0, 0, 0, 0.05);
-}
+    --box-shadow-book--hover: 0 6px 11px -2px rgba(0, 0, 0, 0.12),
+      0 8px 7px -4px rgba(0, 0, 0, 0.09), 0 17px 11px -8px rgba(0, 0, 0, 0.09),
+      0 24px 15px -12px rgba(0, 0, 0, 0.09), 0 4px 15px 0 rgba(0, 0, 0, 0.05);
+  }
+
   *,
   *::before,
   *::after {
@@ -40,8 +48,6 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
-      Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     font: var(--font-body);
   }
 
@@ -51,5 +57,9 @@ export const GlobalStyle = createGlobalStyle`
 
   h1 {
     font: var(--font-headline-1);
+  }
+
+  h1 {
+    font: var(--font-headline-2);
   }
 `;
