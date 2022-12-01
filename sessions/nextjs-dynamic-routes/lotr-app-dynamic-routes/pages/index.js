b951441c03faa30dbec64daee8f461998ea27beb
Jan Willem Henckel <farly.de@gmail.com>
2022-12-01 10:43:38 +0100

    fix files

--- sessions/nextjs-dynamic-routes/lotr-app-dynamic-routes/pages/index.js
+++ sessions/nextjs-dynamic-routes/lotr-app-dynamic-routes/pages/index.js
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Hello from Next.js</h1>
-      <Link href="/books">Go to all books</Link>
+      <Link href="/volumes">Go to all volumes</Link>
     </div>
   );
 }
--- sessions/nextjs-dynamic-routes/lotr-app-dynamic-routes/pages/volumes/the-fellowship-of-the-ring.js
+++ sessions/nextjs-dynamic-routes/lotr-app-dynamic-routes/pages/volumes/the-fellowship-of-the-ring.js
@@ -3,19 +3,28 @@ import Link from "next/link";
 import { volumes } from "../../lib/data.js";
 
 export default function Fellowship() {
-  const currentVolume = volumes.find(({slug}) => slug === "the-fellowship-of-the-ring");
-  
+  const currentVolume = volumes.find(
+    ({ slug }) => slug === "the-fellowship-of-the-ring"
+  );
+
   if (!currentVolume) {
     return null;
   }
-  
+
   const { title, description, cover, books } = currentVolume;
 
   return (
     <>
       <Link href="/volumes">← All Volumes</Link>
-      <h2>{title}</h2>
+      <h1>{title}</h1>
       <p>{description}</p>
+      <ul>
+        {books.map(({ ordinal, title }) => (
+          <li key={title}>
+            {ordinal}: <strong>{title}</strong>
+          </li>
+        ))}
+      </ul>
       <Image
         src={cover}
         alt={`Cover image of ${title}`}
