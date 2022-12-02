3ee89e5b8c771d26bb16ffbc883271c8d33bd95a
PReichetanz <paul.reichetanz@gmx.de>
2022-12-02 14:24:15 +0100

    delete styled version of lotr app

--- sessions/react-styled-components/lotr-app-styling/components/Main/index.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export default styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: ${({ gap = 24 }) => `${gap}px`};
-  padding-block: 48px;
-`;
