903ead0764f2ed24069b28bbefe9f6abfc7d1f1a
PReichetanz <paul.reichetanz@gmx.de>
2022-12-02 12:42:37 +0100

    delete unused components

--- sessions/storybook/presents/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export default function Footer({ children }) {
-  return <StyledFooter>{children}</StyledFooter>;
-}
-
-const StyledFooter = styled.footer`
-  color: var(--color-grey);
-`;
