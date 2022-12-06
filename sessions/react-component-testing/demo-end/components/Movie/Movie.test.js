51dedd15a83e80008f0b62435eb6285a1057f1f2
Jan Willem Henckel <farly.de@gmail.com>
2022-12-06 11:23:37 +0100

    add more tests as examples

--- sessions/react-component-testing/demo-end/components/Movie/Movie.test.js
+++ sessions/react-component-testing/demo-end/components/Movie/Movie.test.js
@@ -1,9 +1,69 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Movie from ".";
 
 test("renders a movie", () => {
   render(<Movie name="The Matrix Reloaded" />);
-  expect(
-    screen.getByRole("heading", { name: "The Matrix Reloaded" })
-  ).toBeInTheDocument();
+  const matrixHeading = screen.getByRole("heading", {
+    name: "The Matrix Reloaded",
+  });
+  expect(matrixHeading).toBeInTheDocument();
+});
+
+test("renders a movie with a like button", () => {
+  render(<Movie name="The Matrix Reloaded" />);
+  const matrixLikeButton = screen.getByRole("button", {
+    name: "like The Matrix Reloaded",
+  });
+  expect(matrixLikeButton).toBeInTheDocument();
+});
+
+test("renders a movie with a unlike button", () => {
+  render(<Movie name="The Matrix Reloaded" isLiked={true} />);
+  const matrixUnlikeButton = screen.getByRole("button", {
+    name: "unlike The Matrix Reloaded",
+  });
+  expect(matrixUnlikeButton).toBeInTheDocument();
+});
+
+test("renders a movie with a delete button", () => {
+  render(<Movie name="The Matrix Reloaded" />);
+  const matrixDeleteButton = screen.getByRole("button", {
+    name: "delete The Matrix Reloaded",
+  });
+  expect(matrixDeleteButton).toBeInTheDocument();
+});
+
+test("calls the onToggleLike handler with the id when the like button is clicked", async () => {
+  const user = userEvent.setup();
+  const handleToggleLike = jest.fn();
+  render(
+    <Movie
+      id="1337"
+      name="The Matrix Reloaded"
+      onToggleLike={handleToggleLike}
+    />
+  );
+  const matrixLikeButton = screen.getByRole("button", {
+    name: "like The Matrix Reloaded",
+  });
+  await user.click(matrixLikeButton);
+  expect(handleToggleLike).toHaveBeenCalledWith("1337");
+});
+
+test("calls the onDeleteMovie handler with the id when the delete button is clicked", async () => {
+  const user = userEvent.setup();
+  const handleDeleteMovie = jest.fn();
+  render(
+    <Movie
+      id="1337"
+      name="The Matrix Reloaded"
+      onDeleteMovie={handleDeleteMovie}
+    />
+  );
+  const matrixDeleteButton = screen.getByRole("button", {
+    name: "delete The Matrix Reloaded",
+  });
+  await user.click(matrixDeleteButton);
+  expect(handleDeleteMovie).toHaveBeenCalledWith("1337");
 });
