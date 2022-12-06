9889160192adef5d2f91e8f04553387dd4f66f6c
Alex Claes <hello@aclaes.com>
2022-12-06 10:09:24 +0100

    Update sessions/react-component-testing/scorekeeper/components/Navigation/index.js

--- sessions/react-component-testing/scorekeeper/components/Navigation/index.js
+++ sessions/react-component-testing/scorekeeper/components/Navigation/index.js
@@ -5,7 +5,6 @@ import { useRouter } from "next/router";
 
 export default function Navigation({ players }) {
   const router = useRouter();
-  console.log(router);
   return (
     <Nav>
       <StyledLink
