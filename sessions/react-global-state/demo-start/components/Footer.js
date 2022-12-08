eadee1c53b4b998a763e8fcf20e36f75d6b17588
CJAkkuec <90191489+CJAkkuec@users.noreply.github.com>
2022-12-08 11:26:22 +0100

    Update sessions/react-global-state/demo-start/components/Footer.js
    Co-authored-by: Alex Claes <hello@aclaes.com>

--- sessions/react-global-state/demo-start/components/Footer.js
+++ sessions/react-global-state/demo-start/components/Footer.js
@@ -1,6 +1,5 @@
-const dragonCount = "???";
-
 export default function Footer() {
+  const dragonCount = "???";
   return (
     <span>
       Counting since 1899 — Currently there are <strong>{dragonCount}</strong>{" "}
