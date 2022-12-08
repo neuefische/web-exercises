df2472f5aad623939478bdd9e3730ec6ef2278e0
CJAkkuec <90191489+CJAkkuec@users.noreply.github.com>
2022-12-08 11:26:11 +0100

    Update sessions/react-global-state/demo-start/components/CounterStats.js
    Co-authored-by: Alex Claes <hello@aclaes.com>

--- sessions/react-global-state/demo-start/components/CounterStats.js
+++ sessions/react-global-state/demo-start/components/CounterStats.js
@@ -1,8 +1,8 @@
-const countAverage = "???";
-const countSum = "???";
-const dragonCount = "???";
-
 export default function CounterStats() {
+  const countAverage = "???";
+  const countSum = "???";
+  const dragonCount = "???";
+
   return (
     <div>
       <h2>Counter Stats</h2>
