0df1bec2df3ea46c7cd0decc4a4f45dd29e30d44
Jan Willem Henckel <farly.de@gmail.com>
2022-12-08 12:12:27 +0100

    fix demo end

--- sessions/react-immutable-state/demo-end/components/Show/index.js
+++ sessions/react-immutable-state/demo-end/components/Show/index.js
@@ -1,6 +1,10 @@
+import { useImmer } from "use-immer";
+// import { useState } from "react";
 import styled from "styled-components";
+import Episode from "../Episode";
+import Season from "../Season";
 
-const Title = styled.h2`
+const Title = styled.h1`
   text-decoration: underline;
   font-size: 1.7rem;
   text-decoration-color: var(--nemo);
@@ -10,17 +14,79 @@ const Title = styled.h2`
   opacity: 1;
 `;
 
-const StyledShow = styled.section`
+const StyledShow = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 24px;
+  gap: 32px;
 `;
 
-export default function Show({ name, children }) {
+export default function Show({ initialSeasons = [] }) {
+  const [seasons, updateSeasons] = useImmer(initialSeasons);
+
+  function handleToggleHasSeen(seasonNumber, episodeNumber) {
+    // setSeasons((prevSeasons) => {
+    //   const season = prevSeasons.find(({ number }) => number === seasonNumber);
+
+    //   const episode = season.episodes.find(
+    //     ({ number }) => number === episodeNumber
+    //   );
+
+    //   episode.hasSeen = !episode.hasSeen;
+
+    //   console.log(prevSeasons);
+
+    //   return prevSeasons;
+    // });
+
+    // setSeasons((prevSeasons) => {
+    //   return prevSeasons.map((prevSeason) => {
+    //     if (prevSeason.number !== seasonNumber) {
+    //       return prevSeason;
+    //     }
+    //     return {
+    //       ...prevSeason,
+    //       episodes: prevSeason.episodes.map((prevEpisode) => {
+    //         if (prevEpisode.number !== episodeNumber) {
+    //           return prevEpisode;
+    //         }
+    //         return {
+    //           ...prevEpisode,
+    //           hasSeen: !prevEpisode.hasSeen,
+    //         };
+    //       }),
+    //     };
+    //   });
+    // });
+
+    updateSeasons((draft) => {
+      const season = draft.find(({ number }) => number === seasonNumber);
+
+      const episode = season.episodes.find(
+        ({ number }) => number === episodeNumber
+      );
+
+      episode.hasSeen = !episode.hasSeen;
+    });
+  }
+
   return (
     <StyledShow>
-      <Title>{name}</Title>
-      {children}
+      <Title>A Series of Unfortunate Events</Title>
+      {seasons.map((season) => (
+        <Season key={season.number} number={season.number}>
+          {season.episodes.map((episode) => (
+            <Episode
+              key={episode.number}
+              number={episode.number}
+              title={episode.title}
+              hasSeen={episode.hasSeen}
+              onToggleHasSeen={() => {
+                handleToggleHasSeen(season.number, episode.number);
+              }}
+            />
+          ))}
+        </Season>
+      ))}
     </StyledShow>
   );
 }
