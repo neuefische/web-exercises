db4ce7817a80ca6afa02fc4680f388dc67743549
PReichetanz <paul.reichetanz@gmx.de>
2022-12-08 17:33:42 +0100

    prepare general structure

---
+++ sessions/react-data-fetching/jokes/components/Joke/index.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export default function Joke() {
+  return (
+    <JokeSection>
+      <StyledJoke>hello</StyledJoke>
+    </JokeSection>
+  );
+}
+
+const JokeSection = styled.section`
+  position: relative;
+  width: 80%;
+  margin: 10rem auto;
+  border: 1px solid orange;
+  border-radius: 10px;
+  &::before {
+    position: absolute;
+    content: "😂";
+    font-size: 2rem;
+    top: -1.5rem;
+    left: -1rem;
+  }
+`;
+
+const StyledJoke = styled.p`
+  margin: 1rem;
+  padding: 0.5rem;
+  background-color: var(--color-water-10);
+  border-radius: 10px;
+  box-shadow: 0px 2px 0px 1px var(--color-granite);
+`;
