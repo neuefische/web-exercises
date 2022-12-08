99b3ac3ecb91f8617d707a546c091103b34fa3ee
Ceren Jana Akkuec <cjakkuec@gmail.com>
2022-12-08 11:39:22 +0100

    quick fix

--- sessions/react-global-state/demo-end/components/CounterList.js
+++ sessions/react-global-state/demo-end/components/CounterList.js
@@ -19,8 +19,8 @@ export default function CounterList({ animals, handleAdd, handleSubtract }) {
           <li key={animal.id}>
             <Counter
               animal={animal}
-              handleAdd={handleAdd}
-              handleSubtract={handleSubtract}
+              onAdd={handleAdd}
+              onSubtract={handleSubtract}
             />
           </li>
         ))}
