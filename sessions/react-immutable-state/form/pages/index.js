9cf265a5392f8deb36367657a172c8b68b747d6f
Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
2022-12-08 16:20:24 +0100

    React Immutable State: Form Challenge (#216)
    * add form challenge for immutable state
    
    * delete solution in form component
    
    * Update sessions/react-immutable-state/form/README.md
    
    Co-authored-by: Alex Claes <hello@aclaes.com>
    
    * Update sessions/react-immutable-state/form/README.md
    
    Co-authored-by: Alex Claes <hello@aclaes.com>
    
    * Update sessions/react-immutable-state/form/README.md
    
    Co-authored-by: Alex Claes <hello@aclaes.com>
    
    * Update sessions/react-immutable-state/form/README.md
    
    Co-authored-by: Alex Claes <hello@aclaes.com>
    
    * Update sessions/react-immutable-state/form/README.md
    
    Co-authored-by: Alex Claes <hello@aclaes.com>
    
    * Update sessions/react-immutable-state/form/components/Form/index.js
    
    Co-authored-by: Alex Claes <hello@aclaes.com>
    
    * Update sessions/react-immutable-state/form/README.md
    
    Co-authored-by: Alex Claes <hello@aclaes.com>
    
    Co-authored-by: Alex Claes <hello@aclaes.com>

---
+++ sessions/react-immutable-state/form/pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+import Form from "../components/Form";
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <h1>Mountain Values</h1>
+      <Form />
+    </Layout>
+  );
+}
