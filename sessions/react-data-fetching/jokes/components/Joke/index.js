5e4a786227d7e78fa465a8d35d7028d2da56b63b
PReichetanz <paul.reichetanz@gmx.de>
2022-12-09 11:50:29 +0100

    finish tasks

--- sessions/react-data-fetching/jokes/components/Joke/index.js
+++ sessions/react-data-fetching/jokes/components/Joke/index.js
@@ -1,20 +1,11 @@
 import { JokeSection, StyledJoke } from "./Joke.styled";
-import useSWR from "swr";
-
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Joke() {
-  const { data, error } = useSWR(
-    "https://example-apis.vercel.app/api/bad-jokes/random",
-    fetcher
-  );
-  console.log(data);
-
-  if (error) return <div>Failed to load {data.status}</div>;
-  if (!data) return <div>Loading...</div>;
   return (
     <JokeSection>
-      <StyledJoke>{data.joke}</StyledJoke>
+      <StyledJoke>
+        I saw a sign that said falling rocks, so I tried and it doesn’t.
+      </StyledJoke>
     </JokeSection>
   );
 }
