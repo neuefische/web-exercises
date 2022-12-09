1df93a6024b319cce96a1db00a2d02edb2a24f02
PReichetanz <paul.reichetanz@gmx.de>
2022-12-09 11:56:33 +0100

    change direction of winner emoji

--- sessions/react-immutable-state/car-race/components/Winner/index.js
+++ sessions/react-immutable-state/car-race/components/Winner/index.js
@@ -1,10 +1,17 @@
+import styled from "styled-components";
+
 export default function Winner({ winner, onRestart }) {
   return (
     <div>
       <h1>
-        <span>{winner.emoji}</span> won!
+        <WinnerEmoji>{winner.emoji}</WinnerEmoji> won!
       </h1>
       <button onClick={() => onRestart()}>Restart</button>
     </div>
   );
 }
+
+const WinnerEmoji = styled.span`
+  transform: scaleX(-1);
+  display: inline-block;
+`;
