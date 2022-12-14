d9c1f73f70c65a048803269528e9b6384d6bc71c
Jan Willem Henckel <farly.de@gmail.com>
2022-12-14 13:04:45 +0100

    Update sessions/react-data-fetching/demo-end/pages/_app.js
    Co-authored-by: Alex Claes <hello@aclaes.com>

--- sessions/react-data-fetching/demo-end/pages/_app.js
+++ sessions/react-data-fetching/demo-end/pages/_app.js
@@ -5,9 +5,11 @@ const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function App({ Component, pageProps }) {
   return (
-    <SWRConfig value={{ fetcher }}>
+    <>
       <GlobalStyle />
-      <Component {...pageProps} />
-    </SWRConfig>
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
+    </>
   );
 }
