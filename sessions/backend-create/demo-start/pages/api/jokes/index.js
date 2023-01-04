73f502c615d6498c9739a6732e13053f29fa51ef
Ceren Jana Akkuec <cjakkuec@gmail.com>
2023-01-04 14:04:22 +0100

    create backend-create demo

---
+++ sessions/backend-create/demo-start/pages/api/jokes/index.js
@@ -0,0 +1,11 @@
+import dbConnect from "../../../db/connect";
+import Joke from "../../../db/models/Joke";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const jokes = await Joke.find();
+    return response.status(200).json(jokes);
+  }
+}
