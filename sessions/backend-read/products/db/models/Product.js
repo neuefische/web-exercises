19057ea9a36664923c802ae7a359db09593d0820
PReichetanz <paul.reichetanz@gmx.de>
2023-01-04 16:43:02 +0100

    add first draft of bonus part

--- sessions/backend-read/products/db/models/Product.js
+++ sessions/backend-read/products/db/models/Product.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import "./Review";
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
@@ -6,6 +7,7 @@ const productSchema = new Schema({
   description: { type: String, required: true },
   price: { type: Number, required: true },
   currency: { type: String, required: true },
+  reviews: { type: [Schema.Types.ObjectId], ref: "Review" },
 });
 
 const Product =
--- sessions/backend-read/products/pages/api/products/[id].js
+++ sessions/backend-read/products/pages/api/products/[id].js
@@ -6,7 +6,7 @@ export default async function handler(request, response) {
   const { id } = request.query;
 
   if (request.method === "GET") {
-    const product = await Product.findById(id);
+    const product = await Product.findById(id).populate("reviews");
 
     if (!product) {
       return response.status(404).json({ status: "Not Found" });
