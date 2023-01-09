d0187064b5e1ae7c082b71a7f720d9f5e7caf70c
Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
2023-01-09 10:32:59 +0100

    Backend Create: Products Challenge (#245)
    * setup backend-read products challenge
    
    * add product overview with connection to database
    
    * finish details page and styling
    
    * add first draft of bonus part
    
    * finish products challenge end version
    
    * add starter for products challenge
    
    * move styled components to separate files
    
    - needed to meet the status of the backend-read challenge
    
    * add create form, route, and styling
    
    * update resources with correct currency
    
    * finish backend-read products challenge
    
    * update files according to review

---
+++ sessions/backend-create/products/components/ProductForm/index.js
@@ -0,0 +1,38 @@
+import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
+import { StyledButton } from "../Button/Button.styled";
+
+export default function ProductForm() {
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+  }
+
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledHeading>Add a new Fish</StyledHeading>
+      <StyledLabel htmlFor="name">
+        Name:
+        <input type="text" id="name" name="name" />
+      </StyledLabel>
+      <StyledLabel htmlFor="description">
+        Description:
+        <input type="text" id="description" name="description" />
+      </StyledLabel>
+      <StyledLabel htmlFor="price">
+        Price:
+        <input type="number" id="price" name="price" min="0" />
+      </StyledLabel>
+      <StyledLabel htmlFor="currency">
+        Currency:
+        <select id="currency" name="currency">
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+        </select>
+      </StyledLabel>
+      <StyledButton type="submit">Submit</StyledButton>
+    </StyledForm>
+  );
+}
