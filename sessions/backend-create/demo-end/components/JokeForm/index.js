1a301fb9f2cbb5027c1fdc129868c5689592fda6
CJAkkuec <90191489+CJAkkuec@users.noreply.github.com>
2023-01-09 09:47:23 +0100

    Update sessions/backend-create/demo-end/components/JokeForm/index.js
    Co-authored-by: Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>

--- sessions/backend-create/demo-end/components/JokeForm/index.js
+++ sessions/backend-create/demo-end/components/JokeForm/index.js
@@ -30,7 +30,7 @@ export default function JokeForm() {
   }
 
   return (
-    <form id="joke-form" onSubmit={onSubmit}>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="joke-input">Enter a new joke</label>
       <input type="text" id="joke-input" name="joke" />
       <button type="submit">Submit</button>
