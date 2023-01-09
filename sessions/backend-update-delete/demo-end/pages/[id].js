1e5b443be221e7b68e4d973ba2f8884c5a77152e
Jessica Lörs <jessica.loers@gmail.com>
2023-01-09 19:33:55 +0100

    add sendRequest function

--- sessions/backend-update-delete/demo-end/pages/[id].js
+++ sessions/backend-update-delete/demo-end/pages/[id].js
@@ -14,35 +14,32 @@ export default function JokeDetailsPage() {
     push,
   } = router;
 
+  async function sendRequest(url, { arg }) {
+    const response = await fetch(url, {
+      method: "PUT",
+      body: JSON.stringify(arg),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      await response.json();
+    } else {
+      console.error(`Error: ${response.status}`);
+    }
+  }
+
   const { trigger, isMutating } = useSWRMutation(
     `/api/jokes/${id}`,
-    async (url, { arg }) => {
-      const response = await fetch(url, {
-        method: "PUT",
-        body: JSON.stringify(arg),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (response.ok) {
-        await response.json();
-      } else {
-        console.error(`Error: ${response.status}`);
-      }
-    }
+    sendRequest
   );
 
   async function handleEditJoke(event) {
     event.preventDefault();
-
     const formData = new FormData(event.target);
     const jokeData = Object.fromEntries(formData);
-
-    console.log(jokeData);
-
     await trigger(jokeData);
-
     push("/");
   }
 
