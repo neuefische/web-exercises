d62f3a9492250b547d372aa16306817ce51183cd
CJAkkuec <90191489+CJAkkuec@users.noreply.github.com>
2023-01-09 10:09:14 +0100

    Backend Create Demo (#243)
    * create backend-create demo
    
    * Update sessions/backend-create/demo-end/components/Joke/index.js
    
    Co-authored-by: Gregor Adams <1148334+pixelass@users.noreply.github.com>
    
    * Update sessions/backend-create/demo-end/components/JokeForm/index.js
    
    Co-authored-by: Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
    
    * Update sessions/backend-create/demo-end/components/JokeForm/index.js
    
    Co-authored-by: Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
    
    Co-authored-by: Gregor Adams <1148334+pixelass@users.noreply.github.com>
    Co-authored-by: Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>

---
+++ sessions/backend-create/demo-end/pages/index.js
@@ -0,0 +1,11 @@
+import JokeList from "../components/JokeList";
+import JokeForm from "../components/JokeForm";
+
+export default function HomePage() {
+  return (
+    <>
+      <JokeForm />
+      <JokeList />
+    </>
+  );
+}
