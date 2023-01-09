8a8498c28cab44687453cdc1ff267190e7b24c62
PReichetanz <paul.reichetanz@gmx.de>
2023-01-09 14:20:32 +0100

    prepare start for challenge

---
+++ sessions/backend-update-and-delete/products/components/Product/index.js
@@ -0,0 +1,30 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { StyledButton } from "../Button/Button.styled";
+import { ProductCard } from "./Product.styled";
+import Comments from "../Comments";
+
+export default function Product() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data } = useSWR(id ? `/api/products/${id}` : null);
+
+  if (!data) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <ProductCard>
+      <h2>{data.name}</h2>
+      <p>Description: {data.description}</p>
+      <p>
+        Price: {data.price} {data.currency}
+      </p>
+      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+      <StyledButton type="button" onClick={() => router.push("/")}>
+        Back to all
+      </StyledButton>
+    </ProductCard>
+  );
+}
---
+++ sessions/backend-update-and-delete/products/components/ProductList/ProductList.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledHeading = styled.h2`
+  text-align: center;
+  color: var(--color-nemo);
+`;
+
+export const StyledList = styled.ul`
+  list-style-type: none;
+  display: grid;
+  gap: 1rem;
+  justify-items: center;
+  padding: 0;
+`;
