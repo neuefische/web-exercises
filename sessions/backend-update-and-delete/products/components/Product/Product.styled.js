9c17a9bc49ba9e6efd712d491b65051691edeca6
Paul Reichetanz <33567019+PReichetanz@users.noreply.github.com>
2023-01-13 11:41:59 +0100

    Backend Update and Delete: Products Challenge (#249)
    * prepare start for challenge
    
    * add first draft of challenge
    
    - not completely working: useSWRMutation is missing
    - styling
    - where to handle form logic
    - delete still missing
    
    * add working update route
    
    * add first draft of readme
    
    * finish tasks in readme file
    
    * refactor: use  "isLoading" instead of "!data"
    
    * reset challenge to start
    
    * Update sessions/backend-update-and-delete/products/README.md
    
    Co-authored-by: Andrea Hohnsen <94636438+ahohnsen@users.noreply.github.com>
    
    * update readme after code review
    
    * Update sessions/backend-update-and-delete/products/README.md
    
    Co-authored-by: Andrea Hohnsen <94636438+ahohnsen@users.noreply.github.com>
    
    * Update sessions/backend-update-and-delete/products/README.md
    
    Co-authored-by: Andrea Hohnsen <94636438+ahohnsen@users.noreply.github.com>
    
    * update after review
    
    * fix typo in readme
    
    Co-authored-by: Andrea Hohnsen <94636438+ahohnsen@users.noreply.github.com>

---
+++ sessions/backend-update-and-delete/products/components/Product/Product.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ProductCard = styled.article`
+  padding: 0.5rem 1rem;
+  box-shadow: 0px 1px 5px -2px var(--color-granite);
+`;
---
+++ sessions/backend-update-and-delete/products/db/models/Product.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import "./Review";
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  currency: { type: String, required: true },
+  reviews: { type: [Schema.Types.ObjectId], ref: "Review" },
+});
+
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
