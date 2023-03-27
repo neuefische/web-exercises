f0c51916f23652c2dc862c57f954cc978772572a
PReichetanz <paul.reichetanz@gmx.de>
2023-03-27 15:02:30 +0200

    fix error message of no env variable is set

--- sessions/recap-project-6/example-implementation/db/connect.js
+++ sessions/recap-project-6/example-implementation/db/connect.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
+    "Please define the MONGODB_URI environment variable inside .env"
   );
 }
 
