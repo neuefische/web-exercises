efd59a13bc9c282eac5fad1af4933f8606160c4f
PReichetanz <paul.reichetanz@gmx.de>
2023-03-27 11:23:33 +0200

    add starter for recap project 6

---
+++ sessions/recap-project-6/tourio-app/components/StyledLink.js
@@ -0,0 +1,24 @@
+import styled, { css } from "styled-components";
+
+export const StyledLink = styled.a`
+  background-color: lightsalmon;
+  padding: 0.8rem 1.5rem;
+  border-radius: 0.6rem;
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+
+  ${({ justifySelf }) =>
+    justifySelf &&
+    css`
+      justify-self: ${justifySelf};
+    `}
+
+  ${({ variant }) =>
+    variant === "outlined" &&
+    css`
+      text-align: center;
+      background-color: white;
+      border: 3px solid lightsalmon;
+    `}
+`;
---
+++ sessions/recap-project-6/tourio-app/pages/create.js
@@ -0,0 +1,27 @@
+import Link from 'next/link.js';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import Form from '../components/Form.js';
+import { StyledLink } from '../components/StyledLink.js';
+
+const StyledBackLink = styled(StyledLink)`
+  justify-self: flex-start;
+`;
+
+export default function CreatePlacePage() {
+  const router = useRouter();
+
+  function addPlace(place) {
+    console.log('Place added (but not really...)');
+  }
+
+  return (
+    <>
+      <h2 id="add-place">Add Place</h2>
+      <Link href="/" passHref legacyBehavior>
+        <StyledBackLink>back</StyledBackLink>
+      </Link>
+      <Form onSubmit={addPlace} formName={'add-place'} />
+    </>
+  );
+}
