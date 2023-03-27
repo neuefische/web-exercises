a2ad83a31529793e9ad22767a05654f4104b03c5
PReichetanz <paul.reichetanz@gmx.de>
2023-03-27 11:56:44 +0200

    add example implementation for recap project 6

---
+++ sessions/recap-project-6/example-implementation/pages/index.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import Card from "../components/Card.js";
+import useSWR from "swr";
+import Link from "next/link.js";
+import { StyledLink } from "../components/StyledLink.js";
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding-left: 0;
+`;
+
+const ListItem = styled.li`
+  position: relative;
+  width: 100%;
+`;
+const FixedLink = styled(StyledLink)`
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+`;
+export default function Home() {
+  const { data } = useSWR("/api/places", { fallbackData: [] });
+
+  return (
+    <>
+      <List role="list">
+        {data.map((place) => {
+          return (
+            <ListItem key={place._id}>
+              <Card
+                name={place.name}
+                image={place.image}
+                location={place.location}
+                id={place._id}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
+      <Link href="/create" passHref legacyBehavior>
+        <FixedLink>+ place</FixedLink>
+      </Link>
+    </>
+  );
+}
