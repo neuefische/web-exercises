77e106b01fbc49e8c9b3f50b9a6b56ee01c7d394
Roland Hufnagel <111574974+Roland-Hufnagel@users.noreply.github.com>
2023-04-11 09:03:03 +0200

    delete @ from next/font/google-path (#284)

--- sessions/react-styled-components/demo-end/styles.js
+++ sessions/react-styled-components/demo-end/styles.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import { Open_Sans } from "@next/font/google";
+import { Open_Sans } from "next/font/google";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
