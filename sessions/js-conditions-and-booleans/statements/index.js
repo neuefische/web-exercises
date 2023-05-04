0a292a7c329ec7424ac4d89c4951a17317ea49a8
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-05-04 15:13:28 +0200

    feat: add challenge for training if else statements

---
+++ sessions/js-conditions-and-booleans/statements/index.js
@@ -0,0 +1,32 @@
+console.clear();
+
+// Part 1: Password
+const SUPER_SECRET_PASSWORD = 'h4x0r1337'
+
+const receivedPassword = 'password1234'
+
+
+// Part 2: Even / Odd
+const number = 6;
+
+
+// Part 3: Hotdogs
+const numberOfHotdogs = 42;
+
+
+// Part 4: Daytime
+const currentHour = 12;
+
+const statement = "";
+
+console.log(statement);
+
+
+// Part 5: Greeting
+const name = "Archibald";
+
+const greeting = "Hello " + (/* write the ternary operator here*/ )+"!";
+
+console.log(greeting);
+
+
