8351003cd0ad233d605ad6b434077774b927262e
Peter Anderson <pete.anderson@gmail.com>
2023-08-25 10:31:45 +0200

    remove unused demo elements

--- sessions/react-styled-components/demo-end/pages/index.js
+++ sessions/react-styled-components/demo-end/pages/index.js
@@ -1,35 +1,14 @@
-import InfoBox from "../components/InfoBox";
 import Button from "../components/Button";
-import Container from "../components/Container";
 import Link from "../components/Link";
 
 export default function HomePage() {
   return (
     <>
-      <Container>
-        <Button>Click me</Button>
-        <Button $color="danger">Click me not</Button>
-      </Container>
+      <Button>Click me</Button>
 
-      <Container>
-        <p>
-          This is a <Link href="/"> link</Link>.
-        </p>
-      </Container>
-
-      <Container $direction="column">
-        <InfoBox title="This is an info" message="Read it carefully!" />
-        <InfoBox
-          title="It worked"
-          message="You have done it!"
-          variant="success"
-        />
-        <InfoBox
-          title="Error"
-          message="Something went wrong!"
-          variant="error"
-        />
-      </Container>
+      <p>
+        This is a <Link href="/"> link</Link>.
+      </p>
     </>
   );
 }
