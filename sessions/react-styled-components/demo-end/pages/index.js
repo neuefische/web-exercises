2f9ecc70fbd75303a986ad62d5ccef26b1dce039
Peter Anderson <pete.anderson@gmail.com>
2023-09-06 16:19:28 +0200

    update demo end to match session guide

--- sessions/react-styled-components/demo-end/pages/index.js
+++ sessions/react-styled-components/demo-end/pages/index.js
@@ -1,5 +1,28 @@
-import Button from "../components/Button";
-import Link from "../components/Link";
+import styled from "styled-components";
+
+const Button = styled.button`
+  all: unset;
+  border: none;
+  cursor: pointer;
+  padding: 1rem;
+  color: white;
+  background-color: var(--primary-color);
+
+  &:hover {
+    color: black;
+    background-color: var(--primary-background);
+  }
+`;
+
+const Link = styled(Link)`
+  font-weight: bold;
+  text-decoration: none;
+  color: var(--primary-color);
+
+  &:hover {
+    color: var(--secondary-color);
+  }
+`;
 
 export default function HomePage() {
   return (
@@ -7,7 +30,7 @@ export default function HomePage() {
       <Button>Click me</Button>
 
       <p>
-        This is a <Link href="/"> link</Link>.
+        This is a <Link href="/">link</Link>.
       </p>
     </>
   );
