1b5f007ce939efcb4bd36a1f3be830964efb4c6e
Diego Caponera <diego.caponera@gmail.com>
2023-10-11 15:15:37 +0200

    Fix PUT.

--- examples/next-data-modelling/pages/_app.js
+++ examples/next-data-modelling/pages/_app.js
@@ -1,20 +1,18 @@
-import useSWR, { SWRConfig } from 'swr';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
-import fetcher from '../lib/fetcher';
-import '../styles/global.css';
+import useSWR, { SWRConfig } from "swr";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import fetcher from "../lib/fetcher";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  const { data: users } = useSWR('/api/users', fetcher);
+  const { data: users } = useSWR("/api/users", fetcher);
   return (
-    <>
-      <SWRConfig value={{ fetcher }}>
-        <Nav />
-        <main>
-          <Component {...pageProps} users={users || []} />
-        </main>
-        <Footer />
-      </SWRConfig>
-    </>
+    <SWRConfig value={{ fetcher }}>
+      <Nav />
+      <main>
+        <Component {...pageProps} users={users || []} />
+      </main>
+      <Footer />
+    </SWRConfig>
   );
 }
--- examples/next-data-modelling/pages/api/posts/[id].js
+++ examples/next-data-modelling/pages/api/posts/[id].js
@@ -1,35 +1,36 @@
-import connect from '@/db/connect.js';
-import Post from '@/db/models/Post.js';
+import connect from "@/db/connect.js";
+import Post from "@/db/models/Post.js";
 
 export default async function handler(request, response) {
   await connect();
-  if (request.method === 'GET') {
+  if (request.method === "GET") {
     const post = await Post.findById(request.query.id)
-      .populate('author')
+      .populate("author")
       .exec();
     if (!post) {
-      response.status(404).json({ message: 'Post not found' });
+      response.status(404).json({ message: "Post not found" });
       return;
     }
     response.status(200).json(post);
     return;
   }
 
-  if (request.method === 'PUT') {
+  if (request.method === "PUT") {
     const post = await Post.findOneAndUpdate(
       {
         _id: request.query.id,
       },
-      request.body
+      request.body,
+      { new: true }
     );
 
     if (!post) {
-      response.status(404).json({ message: 'Post not found' });
+      response.status(404).json({ message: "Post not found" });
       return;
     }
     response.status(200).json(post);
     return;
   }
 
-  response.status(405).json({ message: 'Method not allowed' });
+  response.status(405).json({ message: "Method not allowed" });
 }
