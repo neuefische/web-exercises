6711a8430657558c255231a088e7a705e918b7a4
Diego Caponera <diego.caponera@gmail.com>
2023-10-18 10:42:43 +0200

    Add missing tests for the format function.

--- examples/testing/src/utils.test.js
+++ examples/testing/src/utils.test.js
@@ -3,7 +3,17 @@ import { formatUser } from "./utils";
 
 describe("formatUser", () => {
   it("returns Mr. X if neither the firstName and the lastName are passed", () => {
-    const output = formatUser({ id: 1 });
+    const output = formatUser({});
     expect(output).toBe("Mr. X");
+  });
+
+  it("returns the first name if no last name is given", () => {
+    const output = formatUser({ firstName: "Jane" });
+    expect(output).toBe("Jane");
+  });
+
+  it("returns the first name and the last name (joined with a space) if both are given", () => {
+    const output = formatUser({ firstName: "Jane", lastName: "Doe" });
+    expect(output).toBe("Jane Doe");
   });
 });
