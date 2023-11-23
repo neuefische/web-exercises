70416165d9b7856c434aaf05ba94c94dc7afd45f
Diego Caponera <diego.caponera@gmail.com>
2023-11-23 14:09:13 +0100

    Fix promise try/catch scenario (#338)

--- sessions/js-error-handling/fetch-solution/js/index.js
+++ sessions/js-error-handling/fetch-solution/js/index.js
@@ -4,16 +4,23 @@ const userElement = document.querySelector(".user");
 
 async function getUser(url) {
   const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("User not Found!");
+  }
   const json = await response.json();
   return json.data;
 }
 
 document.querySelectorAll("button[data-url]").forEach((button) =>
   button.addEventListener("click", async (event) => {
-    const user = await getUser(event.target.dataset.url);
-    userElement.innerHTML = `
+    try {
+      const user = await getUser(event.target.dataset.url);
+      userElement.innerHTML = `
     <h2>${user.first_name} ${user.last_name}</h2>
     <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
     `;
+    } catch (error) {
+      alert(error);
+    }
   })
 );
