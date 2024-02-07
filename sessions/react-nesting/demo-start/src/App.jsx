22a35d9b53274f5c493c21a218058830185020d4
KlausStille <mail@klausstille.com>
2024-02-07 16:39:48 +0100

    updated code base

--- sessions/react-nesting/demo-start/src/App.jsx
+++ sessions/react-nesting/demo-start/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Animal({ emoji, name }) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <h2>
+      {emoji} {name}
+    </h2>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <main>
+      <h1>Animal Shelter</h1>
+      <p>Please adopt our lovely pets!</p>
+      <Animal emoji="🐕" name="Lucky" />
+      <Animal emoji="🐈" name="Mittens" />
+      <Animal emoji="🐢" name="Archibald" />
+      <Animal emoji="🐣" name="Chick Norris" />
+    </main>
+  );
+}
