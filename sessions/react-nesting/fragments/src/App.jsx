efcf7b1b1cc92f2e48400c06861c359f1c5f3b72
KlausStille <mail@klausstille.com>
2024-02-07 16:17:50 +0100

    updated codebase

--- sessions/react-nesting/fragments/src/App.jsx
+++ sessions/react-nesting/fragments/src/App.jsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Box from "./components/Box";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="flex-container">
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+    </main>
+  );
 }
-
-export default App
---
+++ sessions/react-nesting/fragments/src/components/Box.jsx
@@ -0,0 +1,4 @@
+export default function Box({ color }) {
+  return <div className="box" style={{ backgroundColor: color }}></div>;
+}
+
