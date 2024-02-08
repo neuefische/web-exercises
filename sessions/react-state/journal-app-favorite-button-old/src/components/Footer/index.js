b93853bc26e7cbac436ccaa1609b3f59c9152e01
KlausStille <mail@klausstille.com>
2024-02-08 10:15:14 +0100

    updated code

--- sessions/react-state/journal-app-favorite-button-old/src/components/Footer/index.js
+++
@@ -1,5 +0,0 @@
-import "./Footer.css";
-
-export default function Footer() {
-  return <footer className="footer">Journal App — 2028</footer>;
-}
--- sessions/react-state/journal-app-favorite-button-old/src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import "./Header.css";
-
-export default function Header() {
-  return (
-    <header className="header">
-      <h1 className="header__title">Journal</h1>
-    </header>
-  );
-}
