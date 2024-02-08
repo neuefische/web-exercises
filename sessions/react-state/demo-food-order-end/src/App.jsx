2ff56e3ced3a3f4cd1a24e4ec5026bbc8ecdeba0
KlausStille <mail@klausstille.com>
2024-02-08 10:46:04 +0100

    updated code

--- sessions/react-state/demo-food-order-end/src/App.jsx
+++ sessions/react-state/demo-food-order-end/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  return (
+    <>
+      <h1>Place your order</h1>
+      <FoodOrder />
+    </>
+  );
+}
+
+function FoodOrder() {
+  const [food, setFood] = useState("?");
+  const [vegan, setVegan] = useState(true);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setFood("🍔")}>
+          <span role="img" aria-label="A hamburger">
+            🍔
+          </span>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setFood("🍕")}>
+          <span role="img" aria-label="A pizza">
+            🍕
+          </span>
+        </button>
+        <button onClick={() => setFood("🌮")}>
+          <span role="img" aria-label="A taco">
+            🌮
+          </span>
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <p>Selected food: {food}</p>
+      <button onClick={() => setVegan(!vegan)}>
+        Make it vegan:{" "}
+        {vegan ? (
+          <span role="img" aria-label="Thumbs up">
+            👍
+          </span>
+        ) : (
+          <span role="img" aria-label="Thumbs down">
+            👎
+          </span>
+        )}
+      </button>
+    </>
+  );
+}
