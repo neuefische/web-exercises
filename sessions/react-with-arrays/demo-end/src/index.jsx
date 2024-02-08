daf508bbd57880ae2f757d055dae9e9040790481
KlausStille <mail@klausstille.com>
2024-02-08 17:11:16 +0100

    followed vite css structure

--- sessions/react-with-arrays/demo-end/src/index.jsx
+++ sessions/react-with-arrays/demo-end/src/index.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
+import "./index.css";
 
 const rootElement = document.querySelector("#root");
 const root = createRoot(rootElement);
