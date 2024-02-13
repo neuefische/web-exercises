11db2a81b7d86c3df64dbf5ad6a8dba20309a3de
Sven Hermann <sven@spiced-academy.com>
2024-02-13 10:57:39 +0100

    Implement EventListeners for all radio button
    A click handler is attached to every radio input element to show the correct answer by adding a border-bottom and indicate if the users choice was correct by setting the border to either 'green' or 'red'.

--- sessions/js-conditions-and-booleans/truthy-and-falsy/js/index.js
+++ sessions/js-conditions-and-booleans/truthy-and-falsy/js/index.js
@@ -1,2 +1,31 @@
-const truthyOrFalsy = Boolean(3 >= 2);
-console.log("The expression is " + truthyOrFalsy);
+document.querySelectorAll('input[type="radio"]').forEach((element) => {
+  element.addEventListener("click", (event) => {
+    // current radio button element
+    const clickedRadioButton = event.target
+
+    const questionName = clickedRadioButton.name
+
+    const truthyLabel = document.querySelector(`label[for="truthy-${questionName}"]`)
+    const falsyLabel = document.querySelector(`label[for="falsy-${questionName}"]`)
+
+    const clickedLabel = document.querySelector(`label[for="${clickedRadioButton.id}"]`)
+
+    // clickedAnswer as boolean
+    const clickedAnswer = clickedLabel.textContent === "truthy"
+
+    // evaluate the legend text as a boolean
+    const correctAnswer = !!eval(clickedLabel.closest("fieldset").querySelector("legend").textContent)
+
+    // set border color: correct answer -> green, incorrect answer -> red
+    const borderColor = (correctAnswer === clickedAnswer) ? 'green' : 'red'
+
+    // 'underline' the correct answer, indicating if the user was right by the used color
+    if (correctAnswer) {
+        truthyLabel.style = `border-bottom: 3px solid ${borderColor}`
+        falsyLabel.style = "border: none"
+    } else {
+        falsyLabel.style = `border-bottom: 3px solid ${borderColor}`
+        truthyLabel.style = "border: none"
+    }
+  });
+});
