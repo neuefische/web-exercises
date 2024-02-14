d01f119f56769ff3a60a383879e45f1c6009b888
Gime Cz <mgczarnievicz@gmail.com>
2024-02-14 10:06:22 +0100

    add more style

--- sessions/react-effects-and-fetch/demo-start/src/components/Joke.jsx
+++ sessions/react-effects-and-fetch/demo-start/src/components/Joke.jsx
@@ -1,8 +1,10 @@
+import "./Joke.css";
+
 export default function Joke() {
   return (
     <>
       <h1>Insert joke here</h1>
-      <button type="button">Next Joke</button>
+      <button type="button" className="button">Next Joke</button>
     </>
   );
 }
