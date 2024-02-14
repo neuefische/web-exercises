228a0c8e17199af8627224abf131e2bf634aa4d8
KlausStille <mail@klausstille.com>
2024-02-14 09:34:03 +0100

    updated code and removed old challenge

--- sessions/react-with-local-storage/stored-note/src/App.jsx
+++ sessions/react-with-local-storage/stored-note/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import FontSelector from "./components/FontSelector";
+import Note from "./components/Note";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [note, setNote] = useState("");
+  const [font, setFont] = useState("system-ui");
+
+  function handleNoteChange(newNote) {
+    setNote(newNote);
+  }
+
+  function handleFontChange(newFont) {
+    setFont(newFont);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app" style={{ "--font": font }}>
+      <FontSelector font={font} onFontChange={handleFontChange} />
+      <Note note={note} onNoteChange={handleNoteChange} />
+    </div>
+  );
 }
-
-export default App
