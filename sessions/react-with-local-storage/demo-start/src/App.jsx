2591f43d6b4568ba4d1a1a19d09655340b514fb9
KlausStille <mail@klausstille.com>
2024-02-14 11:57:06 +0100

    updated code and removed old challenge

--- sessions/react-with-local-storage/demo-start/src/App.jsx
+++ sessions/react-with-local-storage/demo-start/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { uid } from "uid";
+import List from "./components/List";
+import Form from "./components/Form";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  function handleAddTodo(title) {
+    setTodos([
+      {
+        id: uid(),
+        title,
+        isChecked: false,
+      },
+      ...todos,
+    ]);
+  }
+
+  function handleToggleCheckTodo(idToToggle) {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === idToToggle ? { ...todo, isChecked: !todo.isChecked } : todo
+      )
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>Todo-App</h1>
+      <Form onAddTodo={handleAddTodo} />
+      <List todos={todos} onToggleCheckTodo={handleToggleCheckTodo} />
+    </main>
+  );
 }
-
-export default App
