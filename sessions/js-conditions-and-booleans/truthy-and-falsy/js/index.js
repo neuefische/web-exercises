a5442e26dff15ce8f9729ae3e8fac42c48ab7602
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-02-26 15:19:56 +0100

    reworked the challenge

--- sessions/js-conditions-and-booleans/truthy-and-falsy/js/index.js
+++ sessions/js-conditions-and-booleans/truthy-and-falsy/js/index.js
@@ -1,2 +1,13 @@
-const truthyOrFalsy = Boolean(3 >= 2);
-console.log("The expression is " + truthyOrFalsy);
+// You don't have to touch this code.
+// You can solve this exercise in the browser
+
+
+function showAnswers() {
+    console.log("in ShowAnswers()");
+    const spans = document.querySelectorAll("span");
+    spans.forEach((span) => {
+      span.classList.toggle("hidden");
+    });
+  }
+  const button = document.querySelector("[data-js='show-answers']");
+  button.addEventListener("click", showAnswers);
