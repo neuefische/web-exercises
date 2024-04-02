8fb4a88c5df617f5a3ff297bf01aac57f5947b1c
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-04-02 10:38:40 +0200

    add solution react effect and fetch pokemon api

---
+++ sessions/react-effects-and-fetch/pokemon-api_solution/src/components/PokemonList.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export default function PokemonList() {
+  const [pokemon, setPokemon] = useState([]);
+
+  async function loadPokemon() {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const data = await response.json();
+      setPokemon(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    loadPokemon();
+  }, []);
+
+  return (
+    <main>
+      <ul>
+        {pokemon.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
