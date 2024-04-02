f0641fe18ec6f743bef315ea5458d2f63bff729f
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-04-02 09:08:23 +0200

    add solution react state 2 colored number

---
+++ sessions/react-state-2/colored-number_solution/src/components/Counter.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import ColoredNumber from "./ColoredNumber";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="counter">
+      <h1>Colored Number</h1>
+      <ColoredNumber value={count} />
+      <div className="counter__buttons-container">
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="increment count"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="decrement count"
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
