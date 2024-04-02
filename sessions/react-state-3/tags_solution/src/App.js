2f3a4d38f2db2b466ba3fde31d7b5998d9d452f9
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-04-02 14:50:47 +0200

    add solution react state 3 tags

--- sessions/react-state-3/tags_solution/src/App.js
+++ sessions/react-state-3/tags_solution/src/App.js
@@ -6,10 +6,19 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    setTags([...tags, newTag]);
+  }
+
+  function handleDeleteTag(tagToDelete) {
+    const mutatedTags = tags.filter((tag) => tag !== tagToDelete);
+    setTags(mutatedTags);
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
