1557ac040d8476dfb839d3863eaf60135548005b
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-04-26 12:01:41 +0200

    add solution js-forms checkbox input

---
+++ sessions/js-forms/checkbox-input_solution/js/index.js
@@ -0,0 +1,31 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+const tosError = document.querySelector('[data-js="tos-error"]');
+const tosCheckbox = document.querySelector('[data-js="tos"]');
+
+function hideTosError() {
+  tosError.setAttribute("hidden", "");
+}
+
+function showTosError() {
+  tosError.removeAttribute("hidden");
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // --v-- write your code here --v--
+  const tosChecked = tosCheckbox.checked;
+
+  if (!tosChecked) {
+    showTosError();
+    return;
+  }
+
+  hideTosError();
+  // --^-- write your code here --^--
+
+  // eslint-disable-next-line no-alert
+  alert("Form submitted");
+});
