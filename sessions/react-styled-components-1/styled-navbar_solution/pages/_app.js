ed35e4fcb809f3d313785c118a93294427aa533e
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-04-26 16:29:58 +0200

    add solution react-styled-components-1 styled navbar

---
+++ sessions/react-styled-components-1/styled-navbar_solution/pages/_app.js
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import GlobalStyle from "../styles";
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  border-bottom: solid 2px grey;
+  padding: 1rem;
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <StyledNav>
+        <StyledList>
+          <li>
+            <StyledLink href="/">Home</StyledLink>
+          </li>
+          <li>
+            <StyledLink href="/about">About</StyledLink>
+          </li>
+          <li>
+            <StyledLink href="/contact">Contact</StyledLink>
+          </li>
+        </StyledList>
+      </StyledNav>
+      <Component {...pageProps} />
+    </>
+  );
+}
