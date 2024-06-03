51665d9dd8c2956e0ddef27d841aadf73c4e9f5e
Felix Kirchhoff <59687330+F-Kirchhoff@users.noreply.github.com>
2024-06-03 14:13:29 +0200

    Fix/react data fetching (#378)
    * simplify demo and data synchronisation
    
    * remove swrConfig from session
    
    * remove funny button from demo start

--- sessions/react-data-fetching/demo-end/pages/index.js
+++ sessions/react-data-fetching/demo-end/pages/index.js
@@ -1,5 +1,61 @@
-import Joke from "../components/Joke";
+import { useState } from "react";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function HomePage() {
-  return <Joke />;
+  const [id, setId] = useState(0);
+  const [funnyJokeIds, setFunnyJokeIds] = useState([]);
+
+  const {
+    data: joke,
+    isLoading,
+    error,
+  } = useSWR(`https://example-apis.vercel.app/api/bad-jokes/${id}`, fetcher);
+
+  function handlePrevJoke() {
+    setId(joke.prevId);
+  }
+
+  function handleNextJoke() {
+    setId(joke.nextId);
+  }
+
+  function handleToggleFunnyJoke(jokeId) {
+    // you can reuse the "isFunny" variable here if you like
+    if (funnyJokeIds.includes(jokeId)) {
+      //joke is in the list, to toggle the state we need to remove it.
+      const updatedFunnyJokeIds = funnyJokeIds.filter((id) => id !== jokeId);
+      setFunnyJokeIds(updatedFunnyJokeIds);
+    } else {
+      //joke is not the list, to toggle the state we need to add it.
+      const updatedFunnyJokeIds = [...funnyJokeIds, jokeId];
+      setFunnyJokeIds(updatedFunnyJokeIds);
+    }
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error || !joke) {
+    return <h1>Error</h1>;
+  }
+
+  return (
+    <>
+      <small>ID: {id}</small>
+      <h1>{joke.joke}</h1>
+      <button type="button" onClick={() => handleToggleFunnyJoke(id)}>
+        {isFunny ? "😂 is Funny" : "😐 is not Funny"}
+      </button>
+      <div>
+        <button type="button" onClick={handlePrevJoke}>
+          ← Prev Joke
+        </button>
+        <button type="button" onClick={handleNextJoke}>
+          Next Joke →
+        </button>
+      </div>
+    </>
+  );
 }
