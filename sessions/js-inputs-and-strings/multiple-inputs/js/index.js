2b313a7a642ee04edec206131ade8b39b405bba4
Feline Huhn <feline.huhn@gmx.de>
2024-06-06 15:47:48 +0200

    update challenge instruction js-inputs-and-strings multiple-inputs

--- sessions/js-inputs-and-strings/multiple-inputs/js/index.js
+++ sessions/js-inputs-and-strings/multiple-inputs/js/index.js
@@ -1,16 +1,28 @@
-/*
-
-Important: Create a fork for each sub challenge!
-Implement the following functionality:
-
-1. On button click: The value of the first input field is copied into the second input field
-
-2. On button click: The value of the first input field is copied in uppercase into the second input field
-
-3. On button click: The values of the two input fields switch.
-
-*/
-
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+/* # Sub-Challenge 1
+Implement the following functionality: The value of the first input field is copied into the second input field on button click */
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
+
+/* # Sub-Challenge 2: 
+First: Comment out the Sub-Challenge 1 code to test this Sub-Challenge.
+
+Implement the following functionality: The value of the first input field is copied in uppercase into the second input field on button click */
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
+
+/* # Sub-Challenge 3. 
+First: Comment out the previous Sub-Challenges to test this Sub-Challenge.
+
+Implement the following functionality: The values of the two input fields switch on button click */
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
--- sessions/js-inputs-and-strings/multiple-inputs_solution/js/index.js
+++ sessions/js-inputs-and-strings/multiple-inputs_solution/js/index.js
@@ -1,31 +1,42 @@
-/*
-Create a fork for each exercise:
-1. Input value of first input should be copied to second input on button click
-2. Input value of first input should be copied in uppercase to second input on button click
-3. Input values should switch on button click: First input should display the value of the second input and vice versa.
-
-*/
-
 const firstInput = document.querySelector('[data-js=first-input]');
 const secondInput = document.querySelector('[data-js=second-input]');
 const button = document.querySelector('[data-js=button]');
 
-// Solution exercise 1:
+/* # Sub-Challenge 1
+Implement the following functionality: The value of the first input field is copied into the second input field on button click */
 
-// button.addEventListener('click', () => {
-//   secondInput.value = firstInput.value
-// });
+// --v-- write your code here --v--
 
-// Solution exercise 2:
+/* button.addEventListener('click', () => {
+  secondInput.value = firstInput.value
+}); */
 
-// button.addEventListener('click', () => {
-//   secondInput.value = firstInput.value.toUpperCase()
-// });
+// --^-- write your code here --^--
 
-// Solution exercise 3:
+/* # Sub-Challenge 2: 
+First: Comment out the Sub-Challenge 1 code to test this Sub-Challenge.
+
+Implement the following functionality: The value of the first input field is copied in uppercase into the second input field on button click */
+
+// --v-- write your code here --v--
+
+/* button.addEventListener('click', () => {
+  secondInput.value = firstInput.value.toUpperCase()
+}); */
+
+// --^-- write your code here --^--
+
+/* # Sub-Challenge 3. 
+First: Comment out the previous Sub-Challenges to test this Sub-Challenge.
+
+Implement the following functionality: The values of the two input fields switch on button click */
+
+// --v-- write your code here --v--
 
 button.addEventListener('click', () => {
   const firstInputValue = firstInput.value;
   firstInput.value = secondInput.value;
   secondInput.value = firstInputValue;
 });
+
+// --^-- write your code here --^--
