32861cf24631536321c84cc30c8da85c5ed340ac
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-07-30 12:14:27 +0200

    adapt demo code

--- sessions/react-state-1/demo-counter-start/src/App.js
+++ sessions/react-state-1/demo-counter-start/src/App.js
@@ -8,10 +8,7 @@ function Counter() {
   return (
     <div className="counter">
       <h2>You have clicked 0 times</h2>
-      <button
-      >
-        Click me
-      </button>
+      <button>Click me</button>
     </div>
   );
 }
