d68a3531a7b66ba852a467f6ec9adddee3845b5a
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-08-26 16:50:37 +0200

    generate index.html

--- scripts/migrate-vite.js
+++ scripts/migrate-vite.js
@@ -6,11 +6,11 @@
   - add type: module
   - add vite, vite-plugin-eslint, vite-plugin-react devDependencies
   - remove react scripts
-3. rename .js files to .jsx 
+3. ✅ rename .js files to .jsx 
 4. create new index.html in root, delete old one in /public
 5. create vite.config.js
 */
-import { $, Glob } from "bun";
+import { $, Glob, write } from "bun";
 import { readdir } from "fs-extra";
 
 const glob = new Glob("src/**/*.js");
@@ -18,18 +18,53 @@ const sessions = ["react-basics"];
 
 for (const session of sessions) {
   const sessionPath = `./sessions/${session}`;
-  const challenges = await readdir(sessionPath);
+  const challenges = (await readdir(sessionPath)).slice(0, 1);
 
-  for (const challenge of challenges.slice(0, 1)) {
+  for (const challenge of challenges) {
     const challengePath = `${sessionPath}/${challenge}`;
     console.log("--------");
     console.log("Path: ", challengePath);
-    const files = [...glob.scanSync(challengePath)];
 
-    files.forEach(async (file) => {
-      const oldPath = `${challengePath}/${file}`;
-      const newPath = oldPath + "x";
-      await $`mv ${oldPath} ${newPath}`;
-    });
+    // rename files
+    // const files = [...glob.scanSync(challengePath)];
+
+    // files.forEach(async (file) => {
+    //   const oldPath = `${challengePath}/${file}`;
+    //   const newPath = oldPath + "x";
+    //   await $`mv ${oldPath} ${newPath}`;
+    // });
+
+    // create new index.html
+    const challengeName = challenge
+      .replaceAll("-", " ")
+      .replaceAll("_", " ")
+      .split(" ")
+      .map((name) => name[0].toUpperCase() + name.slice(1))
+      .join(" ");
+    console.log(challengeName);
+    await write(
+      challengePath + "/index.html",
+      generateIndexHtml(challengeName)
+    );
   }
+}
+
+function generateIndexHtml(name) {
+  return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <link rel="stylesheet" href="styles.css" />
+    <title>${name}</title>
+    <script type="module" src="/src/index.jsx"></script>
+  </head>
+
+  <body>
+    <div id="root" class="root"></div>
+  </body>
+</html>`;
 }
