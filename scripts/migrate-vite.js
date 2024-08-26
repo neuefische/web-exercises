e5634744bd05b72b33236e02fe9bea1214e15da1
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-08-26 17:02:56 +0200

    generate vite.config.js

--- scripts/migrate-vite.js
+++ scripts/migrate-vite.js
@@ -7,11 +7,11 @@
   - add vite, vite-plugin-eslint, vite-plugin-react devDependencies
   - remove react scripts
 3. ✅ rename .js files to .jsx 
-4. create new index.html in root, delete old one in /public
-5. create vite.config.js
+4. ✅ create new index.html in root, delete old one in /public
+5. ✅ create vite.config.js
 */
 import { $, Glob, write } from "bun";
-import { readdir } from "fs-extra";
+import { existsSync, readdir } from "fs-extra";
 
 const glob = new Glob("src/**/*.js");
 const sessions = ["react-basics"];
@@ -25,27 +25,39 @@ for (const session of sessions) {
     console.log("--------");
     console.log("Path: ", challengePath);
 
-    // rename files
-    // const files = [...glob.scanSync(challengePath)];
+    try {
+      // rename files
+      // const files = [...glob.scanSync(challengePath)];
 
-    // files.forEach(async (file) => {
-    //   const oldPath = `${challengePath}/${file}`;
-    //   const newPath = oldPath + "x";
-    //   await $`mv ${oldPath} ${newPath}`;
-    // });
+      // files.forEach(async (file) => {
+      //   const oldPath = `${challengePath}/${file}`;
+      //   const newPath = oldPath + "x";
+      //   await $`mv ${oldPath} ${newPath}`;
+      // });
 
-    // create new index.html
-    const challengeName = challenge
-      .replaceAll("-", " ")
-      .replaceAll("_", " ")
-      .split(" ")
-      .map((name) => name[0].toUpperCase() + name.slice(1))
-      .join(" ");
-    console.log(challengeName);
-    await write(
-      challengePath + "/index.html",
-      generateIndexHtml(challengeName)
-    );
+      // create new index.html
+      const challengeName = challenge
+        .replaceAll("-", " ")
+        .replaceAll("_", " ")
+        .split(" ")
+        .map((name) => name[0].toUpperCase() + name.slice(1))
+        .join(" ");
+      console.log(challengeName);
+      await write(
+        challengePath + "/index.html",
+        generateIndexHtml(challengeName)
+      );
+
+      // remove old index.html
+      if (existsSync(`${challengePath}/public/index.html`)) {
+        await $`rm "${challengePath}/public/index.html"`;
+      }
+
+      // create vite.config.js
+      await write(challengePath + "/vite.config.js", getViteConfig());
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
@@ -67,4 +79,26 @@ function generateIndexHtml(name) {
     <div id="root" class="root"></div>
   </body>
 </html>`;
+}
+
+function getViteConfig() {
+  return `import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "build",
+    },
+    plugins: [react(), eslint()],
+    server: {
+      open: true,
+      port: 3000,
+    },
+    define: {
+      "process.env": {},
+    },
+  };
+});`;
 }
