9f0dd61208c482603e940e1e95e09aa6556051d1
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-08-26 15:31:24 +0200

    simplify vite config

--- sessions/react-basics/demo-start/vite.config.js
+++ sessions/react-basics/demo-start/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
 import eslint from "vite-plugin-eslint";
 
 export default defineConfig(() => {
@@ -8,7 +7,7 @@ export default defineConfig(() => {
     build: {
       outDir: "build",
     },
-    plugins: [react(), eslint(), svgr({ svgrOptions: { icon: true } })],
+    plugins: [react(), eslint()],
     server: {
       open: true,
       port: 3000,
