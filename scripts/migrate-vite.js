dfd4a5985cef7e7c89933a126b49987d96d50e04
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-08-27 16:04:59 +0200

    use script arguments as session name

--- scripts/migrate-vite.js
+++ scripts/migrate-vite.js
@@ -9,95 +9,102 @@
 3. ✅ rename .js files to .jsx
 4. ✅ create new index.html in root, delete old one in /public
 5. ✅ create vite.config.js
-6. ✅create eslintrc.json
+6. ✅ create eslintrc.json
 */
-import { $, Glob, write, file } from "bun";
+import { $, Glob, write, file, argv } from "bun";
 import { existsSync, readdir } from "fs-extra";
 
 const glob = new Glob("src/**/*.js");
-const sessions = ["react-basics", ""];
 
-for (const session of sessions) {
-  const sessionPath = `./sessions/${session}`;
-  const challenges = (await readdir(sessionPath)).slice(0, 1);
+const session = argv[2];
 
-  for (const challenge of challenges) {
-    const challengePath = `${sessionPath}/${challenge}`;
-    console.log("--------");
-    console.log("Path: ", challengePath);
+const sessions = await readdir("./sessions");
 
-    try {
-      // rename files
-      // const files = [...glob.scanSync(challengePath)];
+if (!sessions.includes(session)) {
+  console.error(`${session} no found.`);
+  process.exit(1);
+}
 
-      // files.forEach(async (file) => {
-      //   const oldPath = `${challengePath}/${file}`;
-      //   const newPath = oldPath + "x";
-      //   await $`mv ${oldPath} ${newPath}`;
-      // });
+const sessionPath = `./sessions/${session}`;
+const challenges = await readdir(sessionPath);
 
-      // create new index.html
-      // const challengeName = challenge
-      //   .replaceAll("-", " ")
-      //   .replaceAll("_", " ")
-      //   .split(" ")
-      //   .map((name) => name[0].toUpperCase() + name.slice(1))
-      //   .join(" ");
-      // console.log(challengeName);
-      // await write(
-      //   challengePath + "/index.html",
-      //   generateIndexHtml(challengeName)
-      // );
+for (const challenge of challenges) {
+  const challengePath = `${sessionPath}/${challenge}`;
+  console.log("--------");
+  console.log("Path: ", challengePath);
 
-      // remove old index.html
-      // if (existsSync(`${challengePath}/public/index.html`)) {
-      //   await $`rm "${challengePath}/public/index.html"`;
-      // }
+  try {
+    // rename files
+    const files = [...glob.scanSync(challengePath)];
 
-      // create vite.config.js and .eslintrc.json
-      // await $`cp -f ./scripts/vite-assets/.eslintrc.json ./scripts/vite-assets/vite.config.js ${challengePath}`;
+    files.forEach(async (file) => {
+      const oldPath = `${challengePath}/${file}`;
+      const newPath = oldPath + "x";
+      await $`mv ${oldPath} ${newPath}`;
+    });
 
-      // create package.json
-      const packageFile = await file("./scripts/vite-assets/package.json");
-      const templatePackageJson = await packageFile.json();
+    // create new index.html
+    const challengeName = challenge
+      .replaceAll("-", " ")
+      .replaceAll("_", " ")
+      .split(" ")
+      .map((name) => name[0].toUpperCase() + name.slice(1))
+      .join(" ");
 
-      const currentPackageFile = await file(`${challengePath}/package.json`);
-      const { dependencies, devDependencies, name } =
-        await currentPackageFile.json();
+    await write(
+      challengePath + "/index.html",
+      generateIndexHtml(challengeName)
+    );
 
-      let sanitizedDepedencies = {};
-      const excludedDependencies = [
-        "react",
-        "react-dom",
-        "react-scripts",
-        "web-vitals",
-      ];
-      for (const dependency in dependencies) {
-        if (
-          excludedDependencies.includes(dependency) ||
-          dependency.includes("@testing-library")
-        ) {
-          continue;
-        }
-
-        sanitizedDepedencies[dependency] = dependencies[dependency];
-      }
-      const newPackageJson = {
-        ...templatePackageJson,
-        name,
-        dependencies: {
-          ...templatePackageJson.dependencies,
-          ...sanitizedDepedencies,
-        },
-      };
-      console.log(newPackageJson);
-      await write(
-        `${challengePath}/package.json`,
-        JSON.stringify(newPackageJson, null, 2)
-      );
-    } catch (error) {
-      console.log(error);
+    // remove old index.html
+    if (existsSync(`${challengePath}/public/index.html`)) {
+      await $`rm "${challengePath}/public/index.html"`;
     }
+
+    // create vite.config.js and .eslintrc.json
+    await $`cp -f ./scripts/vite-assets/.eslintrc.json ./scripts/vite-assets/vite.config.js ${challengePath}`;
+
+    // create package.json
+    const packageFile = await file("./scripts/vite-assets/package.json");
+    const templatePackageJson = await packageFile.json();
+
+    const currentPackageFile = await file(`${challengePath}/package.json`);
+    const { dependencies, name } = await currentPackageFile.json();
+
+    let sanitizedDepedencies = {};
+    const excludedDependencies = [
+      "react",
+      "react-dom",
+      "react-scripts",
+      "web-vitals",
+    ];
+
+    for (const dependency in dependencies) {
+      if (
+        excludedDependencies.includes(dependency) ||
+        dependency.includes("@testing-library")
+      ) {
+        continue;
+      }
+
+      sanitizedDepedencies[dependency] = dependencies[dependency];
+    }
+
+    const newPackageJson = {
+      ...templatePackageJson,
+      name,
+      dependencies: {
+        ...templatePackageJson.dependencies,
+        ...sanitizedDepedencies,
+      },
+    };
+
+    await write(
+      `${challengePath}/package.json`,
+      JSON.stringify(newPackageJson, null, 2)
+    );
+  } catch (error) {
+    console.log(error);
   }
 }
 
