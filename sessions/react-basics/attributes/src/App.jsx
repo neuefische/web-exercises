63a1c489dbca7dd26b3923c7810560730f07d19e
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-08-27 16:08:21 +0200

    migrate react-basics

--- sessions/react-basics/attributes/src/App.jsx
+++ sessions/react-basics/attributes/src/App.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <article>Replace me with an article</article>;
 }
