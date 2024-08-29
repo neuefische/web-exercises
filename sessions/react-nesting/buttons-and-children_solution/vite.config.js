c88b0278bbde3d587b3da3bfcbc28df84a8c7b48
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-08-29 12:08:15 +0200

    migrate react-nesting

---
+++ sessions/react-nesting/buttons-and-children_solution/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "build",
+    },
+    plugins: [react(), eslint()],
+    server: {
+      open: true,
+      port: 3000,
+    },
+    define: {
+      "process.env": {},
+    },
+  };
+});
