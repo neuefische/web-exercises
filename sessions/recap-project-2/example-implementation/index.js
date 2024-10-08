158aec2908cb752d77125e1a923b460ac780a6ca
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-10-08 10:42:04 +0200

    add card interactivity

--- sessions/recap-project-2/example-implementation/index.js
+++ sessions/recap-project-2/example-implementation/index.js
@@ -1,5 +1,25 @@
 // Add interactions to first question card
 const card = document.querySelector("[data-js='card'");
-const answerButton = document.querySelector("[data-js='answer-button'");
-const answerDisplay = document.querySelector("[data-js='answer-display'");
-const bookmarkButton = document.querySelector("[data-js='bookmark-button'");
+const answerButton = card.querySelector("[data-js='answer-button'");
+const answerDisplay = card.querySelector("[data-js='answer-display'");
+const bookmarkButton = card.querySelector("[data-js='bookmark-button'");
+
+// Bookmark Interaction
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+// Answer Display Toggling
+answerButton.addEventListener("click", () => {
+  const isAnswerVisible = answerDisplay.classList.contains(
+    "card__answer--active"
+  );
+
+  if (isAnswerVisible) {
+    answerDisplay.classList.remove("card__answer--active");
+    answerButton.textContent = "Show answer";
+  } else {
+    answerDisplay.classList.add("card__answer--active");
+    answerButton.textContent = "Hide answer";
+  }
+});
