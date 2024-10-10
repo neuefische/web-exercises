e9b76d3322b76e4abbbe69960b2033239401969b
KlausStille <mail@klausstille.com>
2024-10-10 17:00:18 +0200

    update endpoint to send empty array if no place is found instead of a 404

--- sessions/recap-project-6/tourio-app/pages/api/comments/[id]/index.js
+++ sessions/recap-project-6/tourio-app/pages/api/comments/[id]/index.js
@@ -2,16 +2,6 @@ import { comments } from "../../../../lib/db_comments.js";
 
 export default function handler(request, response) {
     const { id } = request.query;
-
-    if (!id) {
-        return;
-    }
-
-    const foundComments = comments.filter(({ place }) => place === id);
-
-    if (!foundComments) {
-        return response.status(404).json({ status: "Not found" });
-    }
-
+    const foundComments = comments.filter(({ placeId }) => placeId === id);
     response.status(200).json(foundComments);
 }
