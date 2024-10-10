751d8dac45123898c3bc7dd77bed2e976ee201d4
KlausStille <mail@klausstille.com>
2024-10-10 16:59:30 +0200

    update dummy data: place -> placeId

--- sessions/recap-project-6/tourio-app/lib/db_comments.js
+++ sessions/recap-project-6/tourio-app/lib/db_comments.js
@@ -4,48 +4,48 @@ export const comments = [
         name: "Elbphilharmonizer",
         comment:
             "Even the Elbphilharmonie can't compete with the grandeur of Wasserschloss in Hamburg! This place turns me into a true Elbphilharmonizer. 🏰🎶",
-        place: "0",
+        placeId: "0",
     },
     {
         id: "2",
         name: "AquaVoyager",
         comment:
             "Took a plunge into the depths of Elbtunnel – where the river's secrets unfold as you journey beneath its waves! 🚇💧 #SubaquaticSights",
-        place: "3",
+        placeId: "3",
     },
     {
         id: "3",
         name: "BurgerBachMaestro",
         comment:
             "I took my taste buds on a symphonic journey at Wasserschloss, Hamburg - where the burgers are as harmonious as Bach's compositions! 🍔🎵",
-        place: "4",
+        placeId: "4",
     },
     {
         id: "4",
         name: "LakesideDreamer",
         comment:
             "Found my haven of tranquility by the shores of Alster – where the ripples of serenity echo my thoughts. ☀️🛶 #Reflections",
-        place: "2",
+        placeId: "2",
     },
     {
         id: "5",
         name: "HarmonyHarbor",
         comment:
             "Feeling on top of the musical world at Elbphilharmonie! 🎶✨ The only place where even the waves of the harbor can't resist dancing to the tunes.",
-        place: "0",
+        placeId: "0",
     },
     {
         id: "6",
         name: "GrooveGlobeTrotter",
         comment:
             "Stepped onto Reeperbahn and got caught in a groove that's taking me on a global musical journey! 🌍🎶 #BeatsBeyondBorders",
-        place: "1",
+        placeId: "1",
     },
     {
         id: "7",
         name: "NeonNightcrawler",
         comment:
             "Navigating the neon wonderland of Reeperbahn – where the nights are as vibrant as my imagination! 🌆🎉 #GlowAndGo",
-        place: "1",
+        placeId: "1",
     },
 ];
