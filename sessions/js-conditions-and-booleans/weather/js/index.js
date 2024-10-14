7d28543df33c6125fdcc6c3879008ea6a55f562d
Jessica Lörs <jessica.loers@gmail.com>
2024-10-14 15:28:15 +0200

    add switch challenge: weather

---
+++ sessions/js-conditions-and-booleans/weather/js/index.js
@@ -0,0 +1,5 @@
+// Change this value to test different "weather" conditions.
+const weather = "rainy"
+
+// Adjust the "temperature" value to trigger different messages (for Part 2 only).
+const temperature = 22; 
