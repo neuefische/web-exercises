bc6e93995ec07eab9e9fda6ef89329ce2dcdb750
Niklas Röttger <niklas.roettger@outlook.de>
2024-10-16 10:53:26 +0200

    add gameform tests

--- sessions/react-component-testing/scorekeeper_solution/components/GameForm/GameForm.test.js
+++ sessions/react-component-testing/scorekeeper_solution/components/GameForm/GameForm.test.js
@@ -8,10 +8,71 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders two input fields and a button", () => {});
+test("renders two input fields and a button", () => {
+  render(<GameForm />);
 
-test("renders a form with the accessible name 'Create a new game'", () => {});
+  const gameNameInput = screen.getByLabelText("Name of game");
+  expect(gameNameInput).toBeInTheDocument();
 
-test("submits the correct form data when every field is filled out", async () => {});
+  const playerNameInput = screen.getByLabelText(
+    "Player names, separated by comma"
+  );
+  expect(playerNameInput).toBeInTheDocument();
 
-test("does not submit form if one input field is left empty", async () => {});
+  const button = screen.getByRole("button", { name: "Create game" });
+  expect(button).toBeInTheDocument();
+});
+
+test("renders a form with the accessible name 'Create a new game'", () => {
+  render(<GameForm />);
+
+  const form = screen.getByRole("form", { name: "Create a new game" });
+
+  expect(form).toBeInTheDocument();
+});
+
+test("submits the correct form data when every field is filled out", async () => {
+  const handleCreateGame = jest.fn();
+  const user = userEvent.setup();
+
+  render(<GameForm onCreateGame={handleCreateGame} />);
+
+  const gameNameInput = screen.getByLabelText("Name of game");
+  const playerNameInput = screen.getByLabelText(
+    "Player names, separated by comma"
+  );
+  const button = screen.getByRole("button", { name: "Create game" });
+
+  await user.type(gameNameInput, "First game of the day");
+  await user.type(playerNameInput, "Bernd, Thomas, Erika");
+  await user.click(button);
+
+  expect(handleCreateGame).toHaveBeenCalledWith({
+    playerNames: ["Bernd", "Thomas", "Erika"],
+    nameOfGame: "First game of the day",
+  });
+});
+
+test("does not submit form if one input field is left empty", async () => {
+  const handleCreateGame = jest.fn();
+  const user = userEvent.setup();
+
+  render(<GameForm onCreateGame={handleCreateGame} />);
+
+  const gameNameInput = screen.getByLabelText("Name of game");
+  const playerNameInput = screen.getByLabelText(
+    "Player names, separated by comma"
+  );
+  const button = screen.getByRole("button", { name: "Create game" });
+
+  await user.type(playerNameInput, "Bernd, Thomas, Erika");
+  await user.click(button);
+
+  expect(handleCreateGame).not.toHaveBeenCalled();
+
+  await user.type(gameNameInput, "First game of the day");
+  await user.clear(playerNameInput);
+  await user.click(button);
+
+  expect(handleCreateGame).not.toHaveBeenCalled();
+});
