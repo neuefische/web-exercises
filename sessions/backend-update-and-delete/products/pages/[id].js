39a82bde475389e91cc9010cc0ef0cf8b698e1cc
Felix Kirchhoff <59687330+F-Kirchhoff@users.noreply.github.com>
2024-10-18 15:41:13 +0200

    Update sessions/backend-update-and-delete/products/pages/[id].js
    Co-authored-by: Niklas Röttger <98757391+NikRoe@users.noreply.github.com>

--- sessions/backend-update-and-delete/products/pages/[id].js
+++ sessions/backend-update-and-delete/products/pages/[id].js
@@ -29,7 +29,7 @@ export default function Product() {
   );
 }
 
-export const ProductCard = styled.article`
+const ProductCard = styled.article`
   padding: 0.5rem 1rem;
   box-shadow: 0px 1px 5px -2px var(--color-granite);
 `;
