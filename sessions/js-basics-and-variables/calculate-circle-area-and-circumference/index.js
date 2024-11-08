7665f729ded13e09a9f546af8960e91cff883247
Andrea Hohnsen <a.hohnsen@mail.de>
2024-11-08 14:47:43 +0100

    Fix typos in js-basics-and-variables challenge

--- sessions/js-basics-and-variables/calculate-circle-area-and-circumference/index.js
+++ sessions/js-basics-and-variables/calculate-circle-area-and-circumference/index.js
@@ -5,9 +5,9 @@ How can you calculate the area and circumference of a pizza - oops, I mean a cir
 
 1. Initialize a variable to represent the radius of the circle. For example, you can set `radius` to a specific value, like 5.
 
-2. Calculate the area of the circle using the formula `Math.PI * radius**2`, and store the result in a variable called `circleArea`.
+2. Calculate the area of the circle using the formula `Math.PI * radius ** 2`, and store the result in a variable called `circleArea`.
 
-3. Calculate the circumference of the circle using the formula `Math.PI * radius * 2`, and store the result in a variable called `circumference`.
+3. Calculate the circumference of the circle using the formula `2 * Math.PI * radius`, and store the result in a variable called `circumference`.
 
 4. Display the results by logging them to the console. Use `console.log` to print the area and circumference.
 
--- sessions/js-basics-and-variables/calculate-circle-area-and-circumference__solution/index.js
+++ sessions/js-basics-and-variables/calculate-circle-area-and-circumference__solution/index.js
@@ -5,7 +5,7 @@ How can you calculate the area and circumference of a pizza - oops, I mean a cir
 
 1. Initialize a variable to represent the radius of the circle. For example, you can set `radius` to a specific value, like 5.
 
-2. Calculate the area of the circle using the formula `Math.PI * radius**2`, and store the result in a variable called `circleArea`.
+2. Calculate the area of the circle using the formula `Math.PI * radius ** 2`, and store the result in a variable called `circleArea`.
 
 3. Calculate the circumference of the circle using the formula `2 * Math.PI * radius`, and store the result in a variable called `circumference`.
 
