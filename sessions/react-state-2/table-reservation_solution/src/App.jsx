a43e861c29aeeb8aae288cf674fa96b80462971c
Andrea Hohnsen <a.hohnsen@mail.de>
2024-11-18 16:39:08 +0100

    Update solution to fit to challenge instructions

--- sessions/react-state-2/table-reservation_solution/src/App.jsx
+++ sessions/react-state-2/table-reservation_solution/src/App.jsx
@@ -5,12 +5,20 @@ import "./styles.css";
 export default function App() {
   const [people, setPeople] = useState(0);
 
+  function handleAddPerson() {
+    setPeople(people + 1);
+  }
+
+  function handleRemovePerson() {
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
       <Counter
-        onAddPerson={() => setPeople(people + 1)}
-        onRemovePerson={() => setPeople(people - 1)}
+        onAddPerson={handleAddPerson}
+        onRemovePerson={handleRemovePerson}
       />
       <p>You are going to reserve a table for {people} people.</p>
     </div>
